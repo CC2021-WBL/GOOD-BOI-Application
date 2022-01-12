2cfe84db0c349b09bf1ce1c6ea808f98b2bb74f5
Robert <elgreen@tlen.pl>
2022-01-12 19:21:41 +0100

    feat: add a universal primary and secondary button This button can be used with two variations of colors.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
+import PriSecBtn from "./components/PriSecBtn";
 
 function App() {
   return (
     <div className="App">
+      {/* #24/UI/Primary-secondary-btns - put here for test purposes only */}
+      <PriSecBtn primary text="PRIMARY" />
+      <PriSecBtn secondary text="SECONDARY" />
+      {/* ==== */}
     </div>
   );
 }
---
+++ src/components/PriSecBtn.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  // component "stylowy"
+  display: block;
+  width: 343px;
+  height: 50px;
+  color: ${(props) => (props.primary ? "black" : "whitesmoke")};
+  font-size: 15px;
+  font-weight: 600;
+  background: ${(props) => (props.primary ? "#e3e3e3" : "#2f80ed")};
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PriSecBtn = (props) => {
+  return <Button primary={props.primary}>{props.text}</Button>;
+};
+
+export default PriSecBtn;
