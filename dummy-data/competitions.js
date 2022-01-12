cb13275174a29bc0949eeb03ba097146fdd81b62
Monika Krella <monika.krella@gmail.com>
2022-01-12 19:19:03 +0100

    -added some doggos, competitors, competitions to dummy data -not finished yet

---
+++ dummy-data/competitions.js
@@ -0,0 +1,8 @@
+const competitions = [
+    {
+        kynologiqueDepartment: 'Gdynia',
+        name: 'Gdanskie Zawody o Piertuszke',
+        date: new Date(2022, 3, 22),
+        
+    }
+]
---
+++ dummy-data/competitors.js
@@ -0,0 +1,23 @@
+const competitors = [
+    {
+        nameOfOwner: 'Matylda',
+        surnameOfOwner: 'Borutka',
+        nameOfCompetitor: 'Matylda',
+        surnameOfCompetitor: 'Borutka',
+        city: 'Sfornegacie',
+        street: 'ul.Pszczelna',
+        numberOfHosue: '27/5',
+        postalCode: '50-124'
+    },
+
+      {
+        nameOfOwner: 'Bodzio',
+        surnameOfOwner: 'Rolnik',
+        nameOfCompetitor: 'Eustachy',
+        surnameOfCompetitor: 'Trabka',
+        city: 'Trabki Wielkie',
+        street: 'al Dziurawa',
+        numberOfHosue: '1',
+        postalCode: '33-221'
+    }
+]
