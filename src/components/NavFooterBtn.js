aad6c310d71d603a77b3c89e354e3b1ced4bf308
Robert <elgreen@tlen.pl>
2022-01-12 23:36:52 +0100

    feat: add-nav-footer-btns

---
+++ src/components/NavFooterBtn.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+// If button has attribute "active", then change bottom border and font color,
+// text to be desided on render
+
+const Button = styled.button`
+  box-sizing: border-box;
+  background: transparent;
+  color: ${(props) => (props.active ? "black" : "#7B8794")};
+  height: 38px;
+  width: 61px;
+  font-size: 16px;
+  margin: 0;
+  padding: 0;
+  border: none;
+  border-bottom: ${(props) => (props.active ? "5px solid black" : "none")};
+  &:hover {
+    background: #f5f5f5;
+  }
+`;
+const NavFooterBtn = (props) => {
+  return <Button active={props.active}>{props.text}</Button>;
+};
+
+export default NavFooterBtn;
