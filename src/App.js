e7ee19ca063e095e509102da3d554a1a3049b44d
Tomasz <skretkowskitomasz@gmail.com>
2022-01-12 19:29:40 +0100

    -Cleaned imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import "./App.css";
-import ClassesPage from "./components/pages/ContestClassesPage";
-import ContestsPage from "./components/pages/ContestsPage";
-import CompetitorsPage from "./components/pages/CompetitorsPage";
-import ExcersisesPage from "./components/pages/ExcersisesPage";
-import SingleSummaryPage from "./components/pages/SingleSummaryPage";
 import HomePage from "./components/pages/HomePage";
-import ClassSummaryPage from "./components/pages/ClassSummaryPage";
-import LoginPage from "./components/pages/LoginPage";
 
 function App() {
   return (
