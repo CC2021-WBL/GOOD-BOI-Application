0e037f0eb04f91d4e3c56500d0bfa3b8b006f250
WeronikaBL <weronika@datenexperten.com>
2022-01-14 00:20:44 +0100

    fix: eslint / prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import "./App.css";
-import PriSecBtn from "./components/PriSecBtn";
-import HomePage from "./components/pages/HomePage";
+import './App.css';
+import PriSecBtn from './components/PriSecBtn';
+import HomePage from './components/pages/HomePage';
 
 function App() {
+  const a = 1;
+
   return (
     <div className="App">
       {/* #24/UI/Primary-secondary-btns - put here for test purposes only */}
       <PriSecBtn primary text="PRIMARY" />
       <PriSecBtn secondary text="SECONDARY" />
       {/* ==== */}
+
       <HomePage />
     </div>
   );
 }
 
 export default App;
+// "eslint-config-prettier": "^8.3.0",
+// "eslint-plugin-react-hooks": "^4.3.0",
+// "prettier": "^2.5.1"
