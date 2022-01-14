ca6c240740691b03996e88adfac625a07b9ff196
Tomasz <skretkowskitomasz@gmail.com>
2022-01-14 01:04:20 +0100

    - fix: changed path names

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import ClassesPage from "./components/pages/ClassesPage";
 import ClassSummaryPage from "./components/pages/ClassSummaryPage";
-import CompetitorsPage from "./components/pages/CompetitorsPage";
+import ClassCompetitorsPage from "./components/pages/ClassCompetitorsPage";
 import ContestsPage from "./components/pages/ContestsPage";
 import ExcersisesPage from "./components/pages/ExcersisesPage";
 import HomePage from "./components/pages/HomePage";
@@ -16,10 +16,11 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/contests" element={<ContestsPage />}>
-          <Route path=":contestId/class" element={<ClassesPage />}>
-            <Route path=":classId" element={<CompetitorsPage />}>
-              <Route path="Woof" element={<ExcersisesPage />} />
-              <Route path="Woof/summary" element={<SingleSummaryPage />} />
+          <Route path=":contestId/classes" element={<ClassesPage />}>
+            <Route path=":classId" element={<ClassCompetitorsPage />}>
+              <Route path="excersises" element={<ExcersisesPage />}>
+                <Route path=":dogId/summary" element={<SingleSummaryPage />} />
+              </Route>
               <Route path="leaderboard" element={<ClassSummaryPage />} />
             </Route>
           </Route>
