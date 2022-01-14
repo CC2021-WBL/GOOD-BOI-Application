c7a3f1c5b3210ada5dd2b7d97d7cb3e298033012
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-01-14 14:22:06 +0100

    work in progress

--- src/atoms/NavElement.js
+++ src/atoms/NavElement.js
@@ -1,11 +1,42 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faChevronLeft} from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 
+
+const Element=styled.div`
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+display: flex;
+justify-content: space-between;
+align-items: center;
+width: 100%;
+height: 100px;
+border: 1px solid black;
+/* *{margin:0} */
+h3{color:#AEAEAE;}
+h1{color:#323F4B}
+`
+const Wrapper=styled.div`
+height: 100%;
+width:68px;
+display:flex;
+align-items:center;
+justify-content: space-between;
+position: relative;
+
+.aaa{color:red;}
+`
 const NavElement = (props) => {
     return(
         <>
-            <FontAwesomeIcon icon={faChevronLeft} />
-            <h1>{props.text}</h1>
+        <Element>
+            <Wrapper>
+        <FontAwesomeIcon icon={faChevronLeft} className='aaa' />
+            <h3>wróć</h3>
+            </Wrapper>
+            <h1 color='#323F4B'>{props.text}</h1>
+            </Element>
         </>
     )
 }
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,5 +1,8 @@
+import NavElement from "../../atoms/NavElement";
+
 const HomePage = () => {
-  return <h1>I am a Home Page Component</h1>;
+  // return <h1>I am a Home Page Component</h1>;
+  return <NavElement text ="Contest"/>
 };
 
 export default HomePage;
