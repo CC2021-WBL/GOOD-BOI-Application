cb521c7a597eb6e623d1fcd910e764a423dfeee7
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-01-15 02:28:41 +0100

    Added prop types check

--- src/atoms/BlueLabel.js
+++ src/atoms/BlueLabel.js
@@ -1,9 +1,17 @@
+// import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import React from 'react';
+
 const BlueLabel = (props) => {
-    return(
-        <div>
-            <h2>{props.text}</h2>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>{props.text}</h2>
+    </div>
+  );
+};
+
+BlueLabel.propTypes = {
+  text: PropTypes.string,
+};
 
 export default BlueLabel;
