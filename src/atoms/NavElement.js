d55b474d19185d9ba71be319c2c502c74a7eca41
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-01-15 02:14:18 +0100

    work in progress 2-more styling

--- src/atoms/NavElement.js
+++ src/atoms/NavElement.js
@@ -1,44 +1,74 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faChevronLeft} from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import React from 'react';
+const Element = styled.div`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100px;
 
+  .navText {
+    color: #323f4b;
+    width: 33%;
+    font-size: 19px;
+  }
+`;
 
-const Element=styled.div`
-margin: 0;
-padding: 0;
-box-sizing: border-box;
-display: flex;
-justify-content: space-between;
-align-items: center;
-width: 100%;
-height: 100px;
-border: 1px solid black;
-/* *{margin:0} */
-h3{color:#AEAEAE;}
-h1{color:#323F4B}
-`
-const Wrapper=styled.div`
-height: 100%;
-width:68px;
-display:flex;
-align-items:center;
-justify-content: space-between;
-position: relative;
+const Wrapper = styled.div`
+  height: 100%;
+  width: 33%;
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  position: relative;
 
-.aaa{color:red;}
-`
+  .arrow {
+    color: #cbd2d9;
+    font-size: 30px;
+  }
+
+  .back {
+    font-size: 14px;
+    color: #aeaeae;
+  }
+`;
+
+const Div = styled.div`
+  width: 33%;
+`;
 const NavElement = (props) => {
-    return(
-        <>
-        <Element>
-            <Wrapper>
-        <FontAwesomeIcon icon={faChevronLeft} className='aaa' />
-            <h3>wróć</h3>
-            </Wrapper>
-            <h1 color='#323F4B'>{props.text}</h1>
-            </Element>
-        </>
-    )
-}
+  const { text } = props;
+  //   NavElement.propTypes = {
+  //     props: PropTypes.object,
 
+  //   };
+
+  return (
+    <>
+      <Element>
+        <Wrapper>
+          <FontAwesomeIcon icon={faChevronLeft} className="arrow" />
+          <h3 className="back">wróć</h3>
+        </Wrapper>
+        <h1 className="navText">{text}</h1>
+        <Div></Div>
+      </Element>
+    </>
+  );
+};
+
+NavElement.propTypes = {
+  text: PropTypes.string,
+};
+// Element.propTypes = {
+//   props: PropTypes.shape({
+//     text: PropTypes.string,
+//   }),
+// };
 export default NavElement;
