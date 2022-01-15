7ef4254a7a1ab1dc0a6bd0aed586677dd82b93d5
Robert <elgreen@tlen.pl>
2022-01-16 00:48:58 +0100

    refactor: renamed component

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,25 +1,22 @@
 import styled from "styled-components";
 import LeaderboardBtn from "./LeaderboardBtn";
 
-const LeaderboardMolecule = styled.div`
-  /* background: black; */
+const Wrapper = styled.div`
   margin: 0 auto 0 auto;
   width: 700px;
   display: block;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  /* outline: 2px dotted black; */
 `;
 
 const Leaderboard = (props) => {
   return (
-    <LeaderboardMolecule>
+    <Wrapper>
       <LeaderboardBtn text={props.text} score={props.score} />
       <LeaderboardBtn text={props.text1} score={props.score1} />
       <LeaderboardBtn text={props.text2} score={props.score2} />
-      {/* How to automatically create more contestants on the list?/or less */}
-    </LeaderboardMolecule>
+    </Wrapper>
   );
 };
 export default Leaderboard;
