c835542b3286b1ecd26e2a967fd6484fa9139d45
Tomasz <skretkowskitomasz@gmail.com>
2022-01-16 13:39:38 +0100

    -Fixed input styles migrate to InputFileldStyled, LoginPage styles fixed

---
+++ src/Atoms/InputField/InputFieldStyled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const InputFieldStyled = styled.input`
+  width: 90%;
+  align-self: center;
+  font-family: FontAwesome;
+  color: #7b8794;
+  font-size: 1.2em;
+  height: 48px;
+  border-radius: 8px;
+  border: 1px solid #7b8794;
+  padding-left: 10px;
+`;
+
+export default InputFieldStyled;
--- src/Pages/LoginPage/LoginPageStyled.js
+++ src/Pages/LoginPage/LoginPageStyled.js
@@ -1,24 +1,15 @@
 import styled from 'styled-components';
 
 const StyledForm = styled.form`
-  width: 343px;
   display: flex;
   flex-direction: column;
+  align-items: center;
+
   label {
     color: #323f4b;
     font-size: 0.7em;
     align-self: flex-start;
-    padding: 2em 0 3px 0;
-  }
-
-  input {
-    font-family: FontAwesome;
-    color: #7b8794;
-    font-size: 1.2em;
-    height: 48px;
-    border-radius: 8px;
-    border: 1px solid #7b8794;
-    padding-left: 10px;
+    padding: 2em 0 3px 20px;
   }
 
   .forgot-pass {
