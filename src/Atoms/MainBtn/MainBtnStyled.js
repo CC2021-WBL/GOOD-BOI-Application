fc52cf3690d182ec520b947da93a9ff683a753cc
Robert <elgreen@tlen.pl>
2022-01-16 21:20:20 +0100

    fix: import colors and fonts variables

--- src/Atoms/MainBtn/MainBtnStyled.js
+++ src/Atoms/MainBtn/MainBtnStyled.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-// import COLORS from '../../Styles/varsStyledComponents';
+import COLORS from '../../Styles/varsStyledComponents';
+import FONTS from '../../Styles/fontsStyledComponents';
 
 export const Wrapper = styled.div`
   display: flex;
@@ -11,29 +12,30 @@ const MainBtnStyled = styled.button`
   display: block;
   width: 343px;
   height: 50px;
-  color: ${(props) => (props.primary ? 'whitesmoke' : 'black')};
-  font-size: 15px;
-  font-weight: 600;
+  color: ${(props) =>
+    !props.primary
+      ? !props.secondary
+        ? `${COLORS.grey800}`
+        : `${COLORS.primary601}`
+      : `${COLORS.white}`};
+  ${FONTS.button};
   background: ${(props) =>
     !props.primary
       ? !props.secondary
         ? 'transparent'
-        : '#ACCCF8'
-      : '#2f80ed'};
-  border-radius: 10px;
+        : `${COLORS.primary201}`
+      : `${COLORS.primary501}`};
+  border-radius: 12px;
   display: flex;
   justify-content: center;
   align-items: center;
-
-  // TODO: COLOR vars
-  /* color: $[COLORS.primary201]; -- > to dziala  */
 `;
 // primary color: white ; background: #2f80ed (niebieskie ciemniejszy)
 // secondary color: #1264D1 (ciemnoniebieski); background: #ACCCF8 (jasnoniebieski)
 // trinary color: #323F4B ; background: transparent
-// primary601: '#1264D1',
-// primary501: '#2F80ED',
-// primary201: '#ACCCF8',
-// grey800: '#323F4B',
+// primary601: '#1264D1', ${COLORS.primary601}
+// primary501: '#2F80ED', ${COLORS.primary501}
+// primary201: '#ACCCF8', ${COLORS.primary201}
+// grey800: '#323F4B', ${COLORS.grey800}
 
 export default MainBtnStyled;
