57ac8639516a8b44e640d37aa9878cf61dc387ca
Tomasz <skretkowskitomasz@gmail.com>
2022-01-16 15:27:01 +0100

    -Fixed Font styles changed do consts

--- src/Pages/ForgotPassPage/ForgotPassPageStyled.js
+++ src/Pages/ForgotPassPage/ForgotPassPageStyled.js
@@ -1,28 +1,33 @@
 import styled from 'styled-components';
+import FONTS from '../../Styles/fontsStyledComponents';
+import COLORS from '../../Styles/varsStyledComponents';
 
 const StyledForgotPassForm = styled.form`
   width: 343px;
   display: flex;
   flex-direction: column;
+
   label {
     color: #323f4b;
-    font-size: 0.7em;
+    ${COLORS.grey800};
+    ${FONTS.label_regular};
     align-self: flex-start;
     padding: 2em 0 3px 0;
   }
 
   input {
-    font-family: FontAwesome;
+    font-family: Mulish, FontAwesome;
     color: #323f4b;
-    font-size: 1.2em;
+    ${FONTS.body_semibold};
     height: 48px;
     border-radius: 8px;
-    border: 1px solid #7b8794;
+    border: 1px solid #323f4b;
     padding-left: 10px;
     margin: 0 0 1em 0;
   }
+
   input::placeholder {
-    color: #7b8794;
+    ${COLORS.grey400};
   }
 `;
 
