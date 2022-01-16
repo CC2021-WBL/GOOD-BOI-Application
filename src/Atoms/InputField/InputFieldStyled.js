510d8f80879812d4ec4f45cefce544ff8c21cdcd
Tomasz <skretkowskitomasz@gmail.com>
2022-01-16 22:51:53 +0100

    - Fixed minor Style changes

--- src/Atoms/InputField/InputFieldStyled.js
+++ src/Atoms/InputField/InputFieldStyled.js
@@ -6,12 +6,20 @@ const InputFieldStyled = styled.input`
   font-family: Mulish, FontAwesome;
   width: 95%;
   align-self: center;
-  color: ${COLORS.grey400};
+  color: ${COLORS.grey800};
   ${FONTS.body_semibold};
   height: 48px;
   border-radius: 8px;
   border: 0.5px solid ${COLORS.grey400};
   padding-left: 10px;
+
+  &::placeholder {
+    color: ${COLORS.grey400};
+  }
+  &:focus {
+    border: 0.5px solid transparent;
+    outline: 3px solid ${COLORS.primary201};
+  }
 `;
 
 export default InputFieldStyled;
