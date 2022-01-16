0c89c227c43a519d7c269af85292ef2cc6dddc61
Robert <elgreen@tlen.pl>
2022-01-16 23:08:40 +0100

    fix: add footer to excersizes page

--- src/Pages/ExercisesPage/ExercisesPage.js
+++ src/Pages/ExercisesPage/ExercisesPage.js
@@ -1,14 +1,18 @@
 import CardWrapper from '../../Atoms/CardWrapper/CardWrapper';
 import SpecialButtonsContainer from '../../Molecules/SpecialButtonsContainer';
+import Footer from '../../Molecules/Footer/Footer';
 
 const ExercisesPage = () => {
   return (
-    <CardWrapper>
-      <div>
-        <h1>I am a Excersises Page Component</h1>
-        <SpecialButtonsContainer></SpecialButtonsContainer>
-      </div>
-    </CardWrapper>
+    <>
+      <CardWrapper>
+        <div>
+          <h1>I am a Excersises Page Component</h1>
+          <SpecialButtonsContainer></SpecialButtonsContainer>
+        </div>
+      </CardWrapper>
+      <Footer />
+    </>
   );
 };
 
