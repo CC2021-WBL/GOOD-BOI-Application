011a69b741423245ac67c9b73e686f0244387ed0
Tomasz <skretkowskitomasz@gmail.com>
2022-01-16 15:38:33 +0100

    -Fix Colors changed to consts

--- src/Pages/ForgotPassPage/ForgotPassPageStyled.js
+++ src/Pages/ForgotPassPage/ForgotPassPageStyled.js
@@ -8,8 +8,7 @@ const StyledForgotPassForm = styled.form`
   flex-direction: column;
 
   label {
-    color: #323f4b;
-    ${COLORS.grey800};
+    color: ${COLORS.grey800};
     ${FONTS.label_regular};
     align-self: flex-start;
     padding: 2em 0 3px 0;
@@ -17,17 +16,17 @@ const StyledForgotPassForm = styled.form`
 
   input {
     font-family: Mulish, FontAwesome;
-    color: #323f4b;
+    color: ${COLORS.grey800};
     ${FONTS.body_semibold};
     height: 48px;
     border-radius: 8px;
-    border: 1px solid #323f4b;
+    border: 0.5px solid ${COLORS.grey400};
     padding-left: 10px;
     margin: 0 0 1em 0;
   }
 
   input::placeholder {
-    ${COLORS.grey400};
+    color: ${COLORS.grey400};
   }
 `;
 
