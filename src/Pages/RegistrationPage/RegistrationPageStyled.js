a546f972674b787180e0eda3666158b5e556c80e
Tomasz <skretkowskitomasz@gmail.com>
2022-01-16 16:46:34 +0100

    - Fix styles and minor changes to focus state of input / consts

--- src/Pages/RegistrationPage/RegistrationPageStyled.js
+++ src/Pages/RegistrationPage/RegistrationPageStyled.js
@@ -1,42 +1,51 @@
 import styled from 'styled-components';
+import FONTS from '../../Styles/fontsStyledComponents';
+import COLORS from '../../Styles/varsStyledComponents';
 
 const RegistrationPageStyled = styled.form`
   width: 327px;
   display: flex;
   flex-direction: column;
-  /* align-items: center; */
 
   label {
-    color: #323f4b;
-    font-size: 0.7em;
+    color: ${COLORS.grey800};
+    ${FONTS.label_regular};
     align-self: flex-start;
     padding: 0 0 3px 10px;
   }
 
   input {
-    font-family: FontAwesome;
-    color: #323f4b;
-    font-size: 1.2em;
+    font-family: Mulish, FontAwesome;
+    color: ${COLORS.grey800};
+    ${FONTS.body_semibold};
     height: 48px;
     border-radius: 8px;
-    border: 1px solid #7b8794;
+    border: 0.5px solid ${COLORS.grey400};
     padding-left: 10px;
     margin: 0 0 5px 0;
-  }
 
-  input::placeholder {
-    color: #7b8794;
+    &::placeholder {
+      color: ${COLORS.grey400};
+    }
+
+    &:focus {
+      outline: none;
+      border: 0.5px solid blue;
+    }
   }
 
   .checkboxWrap {
     display: flex;
     align-items: center;
-    font-size: 15px;
+    text-align: left;
+    font-size: 14px;
+    line-height: 15.96px;
+    letter-spacing: 0.2px;
 
     input[type='checkbox'] {
       cursor: pointer;
       width: 30px;
-      margin: 0 5px 0 0;
+      margin: 0 10px 0 0;
       padding: 0 5px;
     }
   }
