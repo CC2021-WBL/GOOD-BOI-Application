f511e35e289bfd21941e9f780f2cc474ea8c6a7e
Tomasz <skretkowskitomasz@gmail.com>
2022-01-16 12:07:18 +0100

    -Fixed id of input file changed to proper one

---
+++ src/Pages/ForgotPassPage/ForgotPassPage.js
@@ -0,0 +1,38 @@
+import CardWrapper from '../../Atoms/CardWrapper/CardWrapper';
+import InputField from '../../Atoms/InputField/InputField';
+import PriSecBtn from '../../Atoms/PriSecBtn/PriSecBtn';
+import { useState } from 'react';
+import StyledForgotPassForm from './ForgotPassPageStyled';
+import { useNavigate } from 'react-router-dom';
+
+const ForgotPassPage = () => {
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const data = { email };
+    console.log(`User data: ${data}`);
+    navigate('/login');
+  };
+
+  return (
+    <CardWrapper>
+      <StyledForgotPassForm onSubmit={submitHandler}>
+        <InputField
+          labelText="Na jaki email wysłać powiadomienie ?"
+          htmlFor="email"
+          type="email"
+          placeholder="&#xf0e0; Email"
+          id="email"
+          required
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <PriSecBtn text="Potwiedź" />
+      </StyledForgotPassForm>
+    </CardWrapper>
+  );
+};
+
+export default ForgotPassPage;
