1f9c28a2c572f215fbd88415497b2ad8e59def88
Monika Krella <monika.krella@gmail.com>
2022-01-16 20:09:26 +0100

    -first steps at work

---
+++ src/Molecules/ContestComponent/contestComponent.js
@@ -0,0 +1,11 @@
+import propTypes from 'prop-types';
+import { useState } from 'react';
+import InfoLabel from '../../Atoms/InButtonLabel/InfoLabel';
+
+const contestComponent = ({ initialName, initialDate, initialCity }) => {
+  const [contestName, setContestName] = useState(initialName);
+  const [date, setDate] = useSate(initialDate);
+  const [city, setCity] = useState(initialCity);
+};
+
+export default contestComponent;
