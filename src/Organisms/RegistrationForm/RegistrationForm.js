1b6a581a8c9610218846b83870d605865530241b
Tomasz <skretkowskitomasz@gmail.com>
2022-01-20 20:47:48 +0100

    - Added: validation logic to rest of the inputs  - Cleaned unnecessary code

--- src/Organisms/RegistrationForm/RegistrationForm.js
+++ src/Organisms/RegistrationForm/RegistrationForm.js
@@ -23,92 +23,77 @@ const RegistrationForm = () => {
           value={formData.email}
           onChange={handleInputChange}
         />
-
         {errors.email && <p>{errors.email}</p>}
-
         <InputField
           labelText="Hasło"
           htmlFor="password"
           type="password"
           id="password"
           placeholder="&#xf023; Hasło"
-          // required
           value={formData.password}
           onChange={handleInputChange}
         />
-
         {errors.password && <p>{errors.password}</p>}
-
         <InputField
           labelText="Powtórz Hasło"
           htmlFor="password"
           type="password"
           id="repeatpass"
           placeholder="&#xf023; Powtórz Hasło"
-          // required
           value={formData.repeatpass}
           onChange={handleInputChange}
         />
-
         {errors.repeatpass && <p>{errors.repeatpass}</p>}
-
         <InputField
           labelText="Imię"
           htmlFor="firstname"
           type="text"
           placeholder="&#xF007; Imię"
           id="firstname"
-          // required
           value={formData.firstname}
           onChange={handleInputChange}
         />
-
-        {/* {errors.firstname && <p>{errors.firstname}</p>} */}
-
+        {errors.firstname && <p>{errors.firstname}</p>}
         <InputField
           labelText="Nazwisko"
           htmlFor="surname"
           type="text"
           placeholder="&#xF007; Nazwisko"
           id="surname"
-          // required
           value={formData.surname}
           onChange={handleInputChange}
         />
-        {/* {errors.surname && <p>{errors.surname}</p>} */}
+        {errors.surname && <p>{errors.surname}</p>}
         <InputField
           labelText="Ulica i nr domu"
           htmlFor="street"
           type="text"
           placeholder="&#xf015; Ulica i nr domu"
           id="street"
-          // required
           value={formData.street}
           onChange={handleInputChange}
         />
-        {/* {errors.street && <p>{errors.street}</p>} */}
+        {errors.street && <p>{errors.street}</p>}
         <InputField
           labelText="Kod Pocztowy"
           htmlFor="zipcode"
           type="text"
           placeholder="&#xf015; Kod Pocztowy"
           id="zipcode"
-          // required
           value={formData.zipcode}
           onChange={handleInputChange}
         />
-        {/* {errors.zipcode && <p>{errors.zipcode}</p>} */}
+        {errors.zipcode && <p>{errors.zipcode}</p>}
         <InputField
           labelText="Miasto"
           htmlFor="city"
           type="text"
           placeholder="&#xf015; Miasto"
           id="city"
-          // required
           value={formData.city}
           onChange={handleInputChange}
         />
-        {/* {errors.city && <p>{errors.city}</p>} */}
+        {errors.city && <p>{errors.city}</p>}
         <CheckboxAgreeField text="Zapoznałem się z regulaminem GOOD BOI i akceptuję jego postanowienia" />
         <MainButton primary text="Zarejestruj się" />
       </FormWrapper>
--- src/Organisms/RegistrationForm/validateData.js
+++ src/Organisms/RegistrationForm/validateData.js
@@ -1,41 +1,63 @@
 const validateData = (formData) => {
+  const {
+    email,
+    password,
+    repeatpass,
+    firstname,
+    surname,
+    street,
+    city,
+    zipcode,
+  } = formData;
+
   let errors = {};
 
-  if (!formData.email) {
+  if (!email) {
     errors.email = 'Email jest wymagany';
-  } else if (
-    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(formData.email)
-  ) {
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
     errors.email = 'Niepoprawny adres email';
   }
 
-  if (!formData.password) {
+  if (!password) {
     errors.password = 'Hasło jest wymagane';
-  } else if (formData.password.length < 4) {
+  } else if (password.length < 4) {
     errors.password = 'Hasło musi mieć więcej niż 4 znaki';
   }
 
-  if (formData.repeatpass.trim() !== formData.password.trim()) {
+  if (repeatpass.trim() !== password.trim()) {
     errors.repeatpass = 'Powtórzone hasło różni się';
   }
 
-  if (!formData.firstname.trim()) {
+  if (!firstname.trim()) {
     errors.firstname = 'Imię jest wymagane';
+  } else if (firstname.length < 4) {
+    errors.firstname = 'Imię musi mieć więcej niż 4 znaki';
   }
 
-  if (!formData.surname.trim()) {
+  if (!surname.trim()) {
     errors.surname = 'Nazwisko jest wymagane';
+  } else if (surname.length < 4) {
+    errors.surname = 'Nazwisko musi mieć więcej niż 4 znaki';
   }
 
-  if (!formData.street) {
+  if (!street) {
     errors.street = 'Podaj Ulicę';
+  } else if (street.length < 4) {
+    errors.street = 'Nazwa ulicy musi mieć więcej niż 4 znaki';
   }
-  if (!formData.zipcode) {
+
+  if (!zipcode) {
     errors.zipcode = 'Podaj Kod Pocztowy';
+  } else if (zipcode.length > 5) {
+    errors.zipcode = 'Kod pocztowy powinien mieć mniej niż 6 znaków';
   }
-  if (!formData.city) {
+
+  if (!city) {
     errors.city = 'Podaj Miasto';
+  } else if (firstname.length < 4) {
+    errors.city = 'Nazwa miasta musi być dluższa niż 4 znaki';
   }
+
   return errors;
 };
 
