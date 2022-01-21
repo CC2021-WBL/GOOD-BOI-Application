042630be628c5c618f638e5fa3b9e22d27c4a2fc
Tomasz <skretkowskitomasz@gmail.com>
2022-01-21 13:34:59 +0100

    - Changed: RolePage changes for continuing flow

--- src/Pages/RolePage/RolePage.js
+++ src/Pages/RolePage/RolePage.js
@@ -1,5 +1,16 @@
+import CardWrapper from '../../Atoms/CardWrapper/CardWrapper';
+import FormWrapper from '../../Atoms/FormWrapper/FormWrapper';
+import MainButton from '../../Atoms/MainButton/MainButton';
+
 const RolePage = () => {
-  return <h1> Choose Your Role!</h1>;
+  return (
+    <CardWrapper>
+      <FormWrapper>
+        <h1> Choose Your Role!</h1>
+        <MainButton text="Obsługa" primary />
+      </FormWrapper>
+    </CardWrapper>
+  );
 };
 
 export default RolePage;
