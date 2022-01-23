f00f8dd0e3b94ba6ba38af4dbca12695b324d0b9
Tomasz <skretkowskitomasz@gmail.com>
2022-01-23 19:53:49 +0100

    - Fix: uncommented useNavigate  hook in contestComponent cleaned Contests Page

--- src/Pages/ContestsPage/ContestsPage.js
+++ src/Pages/ContestsPage/ContestsPage.js
@@ -1,15 +1,12 @@
 import ContestComponent from '../../Molecules/ContestComponent/contestComponent';
 import RANDOM_CONTESTS from '../../Data/Dummy-data/test-data-random-contests';
-import { Link } from 'react-router-dom';
 
 const ContestsPage = () => {
   return (
     <>
       <h1>LISTA KONKURSÓW</h1>
       <h2>WYBIERZ KONKURS</h2>
-      <Link to={`/contests/${RANDOM_CONTESTS[0].id}/classes/`}>
-        <ContestComponent contestId={RANDOM_CONTESTS[0].id} contestIndex={0} />
-      </Link>
+      <ContestComponent contestId={RANDOM_CONTESTS[0].id} contestIndex={0} />
     </>
   );
 };
