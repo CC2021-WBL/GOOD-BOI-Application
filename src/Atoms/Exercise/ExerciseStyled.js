4ed9bf9247c1c2cebe91ef10cce89aa753a052a8
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-01-24 13:05:27 +0100

    Improved css in ExerciseStyled

--- src/Atoms/Exercise/ExerciseStyled.js
+++ src/Atoms/Exercise/ExerciseStyled.js
@@ -4,18 +4,17 @@ import ExerciseCardStyled from '../../Molecules/ExerciseCard/ExerciseCardStyled'
 import COLORS from '../../Styles/varsStyledComponents';
 
 const ExerciseStyled = styled(ExerciseCardStyled)`
-  box-sizing: border-box;
   width: 50%;
-  margin: 0;
-  padding: 0.625rem;
   align-items: center;
-  text-align: left;
+  padding: 0.625rem;
+  margin: 0;
   border-radius: 0.75rem 0 0 0.75rem;
   ${FONTS.body_semibold};
   ${(props) =>
     props.toggle
       ? `background-color: ${COLORS.positive200};`
       : `background-color:  ${COLORS.grey100}; color: ${COLORS.grey400}`}
+  text-align: left;
 `;
 
 export default ExerciseStyled;
