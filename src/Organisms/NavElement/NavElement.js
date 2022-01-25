671cf70fcee835c58d72611cd7adeca364af179b
Tomasz <skretkowskitomasz@gmail.com>
2022-01-25 20:56:26 +0100

    - Fixed: few style changes to Nav element and div wrapper to set hight

--- src/Organisms/NavElement/NavElement.js
+++ src/Organisms/NavElement/NavElement.js
@@ -1,31 +1,32 @@
 import PropTypes from 'prop-types';
 import { BsChevronLeft } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
 import AppLogo from '../../Assets/AppLogo.png';
-import LogoStyled from '../../Atoms/NavElementStyled/LogoStyled';
 import LinkWrapperStyled from '../../Atoms/NavElementStyled/LinkWrapperStyled';
-
+import LogoStyled from '../../Atoms/NavElementStyled/LogoStyled';
 import { NavElementStyled } from './NavElementStyled';
-import { Link } from 'react-router-dom';
 
 const NavElement = ({ text }) => {
   const navigate = useNavigate();
   return (
-    <NavElementStyled>
-      <LinkWrapperStyled onClick={() => navigate(-1)}>
-        <BsChevronLeft className="arrowLeft" />
+    <div style={{ height: '60px' }}>
+      <NavElementStyled>
+        <LinkWrapperStyled onClick={() => navigate(-1)}>
+          <BsChevronLeft className="arrowLeft" />
 
-        <h3 className="back">wróć</h3>
-      </LinkWrapperStyled>
+          <h3 className="back">wróć</h3>
+        </LinkWrapperStyled>
 
-      <h1 className="navText">{text}</h1>
-      <LogoStyled>
-        <Link to="/">
-          <img src={AppLogo} alt="Logo aplikacji" className="logo" />
-        </Link>
-      </LogoStyled>
-    </NavElementStyled>
+        <h1 className="navText">{text}</h1>
+        <LogoStyled>
+          <Link to="/">
+            <img src={AppLogo} alt="Logo aplikacji" className="logo" />
+          </Link>
+        </LogoStyled>
+      </NavElementStyled>
+    </div>
   );
 };
 
--- src/Organisms/NavElement/NavElementStyled.js
+++ src/Organisms/NavElement/NavElementStyled.js
@@ -4,13 +4,14 @@ import COLORS from '../../Styles/varsStyledComponents';
 import FONTS from '../../Styles/fontsStyledComponents';
 
 export const NavElementStyled = styled.div`
+  position: fixed;
   display: flex;
   width: 100%;
   height: 3.75rem;
   align-items: center;
   justify-content: space-between;
   padding: 0 1rem 0 0.563rem;
-  border: 0.5px solid ${COLORS.grey400};
+  border-bottom: 0.5px solid ${COLORS.grey400};
 
   .navText {
     width: 33%;
