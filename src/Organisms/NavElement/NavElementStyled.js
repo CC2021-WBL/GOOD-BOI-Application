55552db87189e16d8fab0a72fef20d029e1a148b
Robert <elgreen@tlen.pl>
2022-01-25 22:16:01 +0100

    fix: fixed stylelint errors

--- src/Organisms/NavElement/NavElementStyled.js
+++ src/Organisms/NavElement/NavElementStyled.js
@@ -1,17 +1,16 @@
-import styled from 'styled-components';
-
 import COLORS from '../../Styles/varsStyledComponents';
 import FONTS from '../../Styles/fontsStyledComponents';
+import styled from 'styled-components';
 
 export const NavElementStyled = styled.div`
   position: fixed;
-  background-color: white;
   display: flex;
   width: 100%;
   height: 3.75rem;
   align-items: center;
   justify-content: space-between;
   padding: 0 1rem 0 0.563rem;
+  background-color: white;
 
   .navText {
     width: 33%;
