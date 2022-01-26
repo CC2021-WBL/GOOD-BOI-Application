11d7941310f8936e526ff13bff4ec0e130b046a1
Monika Krella <monika.krella@gmail.com>
2022-01-26 19:02:01 +0100

    -added modal and navigation from modal, when user send correct data via register form

--- src/Organisms/RegistrationForm/RegistrationFormSuccess.js
+++ src/Organisms/RegistrationForm/RegistrationFormSuccess.js
@@ -1,8 +1,35 @@
+import Backdrop from '../../Atoms/Modal/Backdrop';
+import Modal from '../Modal/Modal';
+import modalData from '../../Consts/modalData';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
 const RegistrationFormSuccess = () => {
+  const [isGoodRegistrationModalOpen, setGoodRegistraionModalOpen] =
+    useState(true);
+  let navigate = useNavigate();
+
+  const handlegoodRegistration = () => {
+    setGoodRegistraionModalOpen(false);
+    navigate(`/login`);
+  };
+
+  function closeModalHandler() {
+    setGoodRegistraionModalOpen(false);
+    navigate(`/`);
+  }
+
   return (
-    <div>
-      <p>Zarejestrowano pomyślnie!</p>
-    </div>
+    <>
+      {isGoodRegistrationModalOpen && (
+        <Modal
+          modalData={modalData.goodRegistration}
+          onCloseHandler={closeModalHandler}
+          onConfirmHandler={handlegoodRegistration}
+        />
+      )}
+      {isGoodRegistrationModalOpen && <Backdrop onClick={closeModalHandler} />}
+    </>
   );
 };
 
