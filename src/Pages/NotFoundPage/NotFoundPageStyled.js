45f8fef47bec109b415955d12c10db2a748cc1c3
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-01-26 14:48:46 +0100

    Stylelint improvements

--- src/Pages/NotFoundPage/NotFoundPageStyled.js
+++ src/Pages/NotFoundPage/NotFoundPageStyled.js
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 import FONTS from '../../Styles/fontsStyledComponents';
 const NotFoundPageWrapper = styled.div`
-  margin: 51px 16px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin: 51px 16px;
 
   .mainContentWrapper {
+    display: flex;
     width: 16.25rem;
     height: 7.5rem;
-    display: flex;
     flex-direction: column;
     justify-content: space-between;
     margin: 35px 0 68px 0;
