f527c25cc2f293f0f53b62e281d071b7245f4e51
Tomasz <skretkowskitomasz@gmail.com>
2022-01-27 21:15:43 +0100

    - Added: EroorBoundary class Component

---
+++ src/Tools/ErrorBoundary.js
@@ -0,0 +1,35 @@
+import propTypes from 'prop-types';
+import { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <Link to="*" />;
+    }
+
+    return this.props.children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: propTypes.node,
+};
+
+export default ErrorBoundary;
