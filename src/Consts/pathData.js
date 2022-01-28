557429c049b6610f6705d3124e6ec15e86fae529
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-01-28 15:30:22 +0100

    work in progress

--- src/Consts/pathData.js
+++ src/Consts/pathData.js
@@ -1,61 +1,88 @@
+// const pathData = [
+//   { path: '/login', text: 'Logowanie' },
+//   { path: '/role', text: 'Wybierz rolę', label: 'cccccccc' },
+//   { path: '/register', text: 'Rejestracja' },
+//   { path: '/forgot', text: 'Resetuj hasło' },
+//   {
+//     path: '/role/contest',
+//     text: 'Lista konkursów',
+//     label: 'Wybierz konkurs',
+//   },
+//   {
+//     path: '/contactForm',
+//     text: 'Formularz kontaktowy',
+//     label: 'Wybierz konkurs',
+//   },
+//   {
+//     path: 'role/contest/1/classes',
+//     text: 'Lista klas',
+//     label: 'XII zawody im. Pana Starosty',
+//   },
+//   {
+//     path: 'role/contest/1/class/1',
+//     text: 'Lista uczestników',
+//     label: 'Klasa 1',
+//   },
+//   {
+//     path: 'role/contest/1/class/1/Woof',
+//     text: 'Punktacja',
+//     label: 'Ocena zawodnika Woof',
+//   },
+//   {
+//     path: 'role/contest/1/class/1/Woof/dogSummary',
+//     text: 'Tabela wyników',
+//     label: 'Ocena zawodnika Woof',
+//   },
+//   { path: '/forgot', text: 'Resetuj hasło' },
+//   {
+//     path: '/contests/closed',
+//     text: 'Lista konkursów',
+//     label: 'Wybierz konkurs',
+//   },
+//   {
+//     path: '/role/contest/1/class/1/classSummary',
+//     text: 'Wyniki w klasie',
+//     label: 'Wyniki w klasie 1',
+//   },
+//   { path: '/userDogs', text: 'Twoje psy' },
+//   { path: '/userContests', text: 'Konkursy' },
+//   { path: '/dogSubmit', text: 'Zgłoś psa' },
+//   { path: '/dogData', text: 'Dane psa' },
+//   { path: '/participantData', text: 'Dane zawodnika' },
+//   { path: '/classChoice', text: 'Wybór klasy', label: 'Dostępne klasy' },
+//   {
+//     path: '/confirmation',
+//     text: 'Potwierdzenie zgłoszenia',
+//     label: 'Status zgłoszenia',
+//   },
+//   { path: '/addDogForm', text: 'Formularz', label: 'Dodaj psa' },
+// ];
+
+// export default pathData;
 const pathData = [
   { path: '/login', text: 'Logowanie' },
-  { path: '/role', text: 'Wybierz rolę', label: 'cccccccc' },
+  { path: '/forgot', text: 'Resetuj hasło' },
   { path: '/register', text: 'Rejestracja' },
-  { path: '/forgot', text: 'Resetuj hasło' },
+  { path: '/role', text: 'Wybierz rolę', label: 'bierta rolę' },
   {
-    path: '/role/contest',
+    path: '/contests',
     text: 'Lista konkursów',
     label: 'Wybierz konkurs',
   },
   {
-    path: '/contactForm',
-    text: 'Formularz kontaktowy',
-    label: 'Wybierz konkurs',
-  },
-  {
-    path: 'role/contest/1/classes',
+    path: '/contests/:id/classes',
     text: 'Lista klas',
-    label: 'XII zawody im. Pana Starosty',
+    label: 'Tu będzie nazwa konkursu',
   },
   {
-    path: 'role/contest/1/class/1',
-    text: 'Lista uczestników',
-    label: 'Klasa 1',
+    path: '/contests/:contestId/classes',
+    text: 'Lista klas',
+    label: 'Tu będzie nazwa konkursu',
   },
   {
-    path: 'role/contest/1/class/1/Woof',
-    text: 'Punktacja',
-    label: 'Ocena zawodnika Woof',
+    path: '/contests/:contestId/classes/:classId',
+    text: 'pieski',
+    label: 'wybierz pieska',
   },
-  {
-    path: 'role/contest/1/class/1/Woof/dogSummary',
-    text: 'Tabela wyników',
-    label: 'Ocena zawodnika Woof',
-  },
-  { path: '/forgot', text: 'Resetuj hasło' },
-  {
-    path: '/contests/closed',
-    text: 'Lista konkursów',
-    label: 'Wybierz konkurs',
-  },
-  {
-    path: '/role/contest/1/class/1/classSummary',
-    text: 'Wyniki w klasie',
-    label: 'Wyniki w klasie 1',
-  },
-  { path: '/userDogs', text: 'Twoje psy' },
-  { path: '/userContests', text: 'Konkursy' },
-  { path: '/dogSubmit', text: 'Zgłoś psa' },
-  { path: '/dogData', text: 'Dane psa' },
-  { path: '/participantData', text: 'Dane zawodnika' },
-  { path: '/classChoice', text: 'Wybór klasy', label: 'Dostępne klasy' },
-  {
-    path: '/confirmation',
-    text: 'Potwierdzenie zgłoszenia',
-    label: 'Status zgłoszenia',
-  },
-  { path: '/addDogForm', text: 'Formularz', label: 'Dodaj psa' },
 ];
-
 export default pathData;
