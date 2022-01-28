5e1430ea48f0f4e05bf13e2b1207840f6fdd5a29
Monika Krella <monika.krella@gmail.com>
2022-01-28 13:46:34 +0100

    -part of modifications of dummy data to prepate it like future data base

---
+++ src/Data/MongoDBMock/contests.js
@@ -0,0 +1,163 @@
+const contests = [
+  {
+    contestId: 'a0347677-c3c9-4edc-9d46-fed4a958fdc2',
+    competitionName: 'XII Zawody im. Pana Starosty',
+    kynologiqueDepartment: 'Gdynia',
+    startDate: new Date(2022, 3, 22, 10, 0),
+    endDate: new Date(2022, 3, 23),
+    place: 'Gdańsk',
+    detailAdress: 'ul.Grunwaldzka 222',
+    judge: 'Alberto Makkaroni',
+    steward: 'Wiesio Kapusta',
+    manager: 'Jagienka Krzywobroda',
+    entryFee: 90,
+    participants: ['matylda1234', 'bogdan678', 'zosia9474'],
+    obedienceClasses: {
+      class0: {
+        isComleted: false,
+        competingPairs: [
+          {
+            dogName: 'Woof',
+            summaryId: '22-03-22Woof',
+          },
+          {
+            dogName: 'Nosek',
+            summaryId: '22-03-22Nosek',
+          },
+          {
+            dogName: 'Ptysio',
+            summaryId: '22-03-22Ptysio',
+          },
+        ],
+      },
+      class1: {
+        isComleted: false,
+        competingPairs: [
+          {
+            dogName: 'Ollie',
+            summaryId: '22-03-22Ollie',
+          },
+          {
+            dogName: 'Norah',
+            summaryId: '22-03-22Norah',
+          },
+          {
+            dogName: 'Zumba',
+            summaryId: '22-03-22Zumba',
+          },
+        ],
+      },
+    },
+  },
+
+  {
+    id: '3845029d-e97d-41ed-997f-2299d09ef648',
+    kynologiqueDepartment: 'Poznań',
+    competitionName: 'Piętnasty zjazd dobrych chłopaków',
+    date: new Date(2022, 5, 1),
+    endDate: new Date(2022, 3, 23),
+    place: 'Poznań',
+    detailAdress: 'ul.Grunwaldzka 222',
+    judge: 'Zuzzana Zezowa',
+    steward: 'Zygfryd Zaduzy',
+    manager: 'Zbyszko Bogdaniec',
+    entryFee: 110,
+    obedienceClasses: {
+      class0: {
+        isComleted: false,
+        competingPairs: [
+          {
+            dogName: 'Woof',
+            summaryId: '22-05-01Woof',
+          },
+          {
+            dogName: 'Nosek',
+            summaryId: '22-05-01Nosek',
+          },
+          {
+            dogName: 'Ptysio',
+            summaryId: '22-05-01Ptysio',
+          },
+        ],
+      },
+      class1: {
+        isComleted: false,
+        competingPairs: [
+          {
+            dogName: 'Ollie',
+            summaryId: '22-05-01Ollie',
+          },
+          {
+            dogName: 'Norah',
+            summaryId: '22-05-01Norah',
+          },
+          {
+            dogName: 'Zumba',
+            summaryId: '22-05-01Zumba',
+          },
+        ],
+      },
+    },
+  },
+  {
+    id: 'c9e7b738-f8e6-4300-9087-332ad5a28b4f',
+    kynologiqueDepartment: 'Sopot',
+    competitionName: 'Konkurs grzeczności',
+    date: new Date(2021, 12, 24, 11, 0),
+    endDate: new Date(2021, 12, 24),
+    place: 'Sopot',
+    detailAdress: 'ul. Potokowa 13',
+    judge: 'Aniela Loczek',
+    steward: 'Bruno Mars',
+    manager: 'Gerwazy Kapusta',
+    entryFee: 70,
+    obedienceClasses: {
+      class0: {
+        isComleted: false,
+        competingPairs: [
+          {
+            dogName: 'Woof',
+            summaryId: '21-12-24Woof',
+          },
+        ],
+      },
+      class1: {
+        isComleted: false,
+        competingPairs: [
+          {
+            dogName: 'Nosek',
+            summaryId: '21-12-24Nosek',
+          },
+        ],
+      },
+      class2: {
+        isComleted: false,
+        competingPairs: [
+          {
+            dogName: 'Ptysio',
+            summaryId: '21-12-24Ptysio',
+          },
+          {
+            dogName: 'Ollie',
+            summaryId: '21-12-24Ollie',
+          },
+        ],
+      },
+      class3: {
+        isComleted: false,
+        competingPairs: [
+          {
+            dogName: 'Norah',
+            summaryId: '21-12-24Norah',
+          },
+          {
+            dogName: 'Zumba',
+            summaryId: '21-12-24Zumba',
+          },
+        ],
+      },
+    },
+  },
+];
+
+export default contests;
---
+++ src/Data/MongoDBMock/participants.js
@@ -0,0 +1,111 @@
+const competitors = [
+  {
+    participantId: 'matylda1234',
+    email: 'matylda@op.pl',
+    password: 'dupabiskupa',
+    participantName: 'Matylda',
+    participantSurname: 'Borutka',
+    country: 'Polska',
+    city: 'Sfornegacie',
+    street: 'ul.Pszczelna',
+    numberOfHosue: '27/5',
+    postalCode: '50-124',
+    dogs: [
+      {
+        dogId: 'VIII-40407',
+        dogName: 'Woof',
+      },
+      {
+        dogId: 'VII-27885',
+        dogName: 'Kluska',
+      },
+    ],
+    portalRoles: ['participant', 'staff'],
+  },
+  {
+    participantId: 'bogdan678',
+    email: 'bogdan777@wp.pl',
+    password: 'rzuckamienia',
+    participantName: 'Bodzio',
+    participantSurname: 'Rolnik',
+    country: 'Polska',
+    city: 'Trabki Wielkie',
+    street: 'al Dziurawa',
+    numberOfHosue: '1',
+    postalCode: '33-221',
+    dogs: [
+      {
+        dogId: 'VIII-27165',
+        dogName: 'Nosek',
+      },
+    ],
+    portalRoles: ['participant'],
+  },
+  {
+    participantId: 'eustachy123',
+    email: 'eustachy123@wp.pl',
+    password: 'mordeczka',
+    participantName: 'Eustachy',
+    participantSurname: 'Trompka',
+    country: 'Polska',
+    city: 'Warszawa',
+    street: 'ul.Gnilna',
+    numberOfHosue: '45/65',
+    postalCode: '40-665',
+    dogs: [
+      {
+        dogId: 'X-37657',
+        dogName: 'Ollie',
+      },
+    ],
+    portalRoles: ['participant'],
+  },
+  {
+    participantId: 'zosia9474',
+    email: 'zosia@wp.pl',
+    password: 'zosiazosia',
+    participantName: 'Zosia',
+    participantSurname: 'Samosia',
+    country: 'Polska',
+    city: 'Warszawa',
+    street: 'ul.Słoneczna',
+    numberOfHosue: '15/1',
+    postalCode: '10-234',
+    dogs: [
+      {
+        dogId: 'V-11165',
+        dogName: 'Ptysio',
+      },
+    ],
+    portalRoles: ['participant'],
+  },
+];
+
+export default competitors;
+
+// .insertOne - dodaje 1 obiekt do kolekcji, przyjmuje obiekt
+// .insertMany - dodaje wiele obiektów do kolekcji, przyjmuje tablicę obiektów
+
+/* db.participans.insertOne({
+  participantId: 'playernhdnjhyvsjnlko32',
+  email: 'bogdan777@wp.pl',
+  password: 'rzuckamienia',
+  participantName: 'Bodzio',
+  participantSurname: 'Rolnik',
+  country: 'Polska',
+  city: 'Trabki Wielkie',
+  street: 'al Dziurawa',
+  numberOfHosue: '1',
+  postalCode: '33-221',
+  dogs: [
+    {
+      dogId: '46576882',
+      dogName: 'Nosek',
+    },
+  ],
+  portalRoles: ['participant'],
+}); */
+
+// db.participants.find() - wyświetli wszystkie obiekty kolekcji
+// db.particpants.find({participantId: "53e823248984"}) - wyświetli wszystkie dane zawodnika pod tym ID
+// db.participants.find({participantId: "53e823248984"}, {dogs: 1})
