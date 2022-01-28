3604235b8f274523456964dbde676f82a7748d48
Smyku6 <smyku6@gmail.com>
2022-01-28 21:22:39 +0100

    little fix for padding-top

--- src/PagesBody/ContestsPage/ContestsPage.js
+++ src/PagesBody/ContestsPage/ContestsPage.js
@@ -4,7 +4,7 @@ import RANDOM_CONTESTS from '../../Data/Dummy-data/test-data-random-contests';
 
 const ContestsPage = () => {
   return (
-    <ColumnWrapper paddingLeftRight={1}>
+    <ColumnWrapper paddingLeftRight={1} paddingTop={0.5}>
       <ContestCard contestId={RANDOM_CONTESTS[0].id} contestIndex={0} />
       <ContestCard contestId={RANDOM_CONTESTS[1].id} contestIndex={1} />
     </ColumnWrapper>
