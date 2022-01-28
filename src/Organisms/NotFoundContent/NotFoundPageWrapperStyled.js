b35dae3b7b4b58537919b08a15c0aa460ec0293a
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-01-28 18:21:30 +0100

    Improved margin

--- src/Organisms/NotFoundContent/NotFoundPageWrapperStyled.js
+++ src/Organisms/NotFoundContent/NotFoundPageWrapperStyled.js
@@ -1,5 +1,6 @@
-import styled from 'styled-components';
 import FONTS from '../../Styles/fontsStyledComponents';
+import styled from 'styled-components';
+
 const NotFoundPageWrapperStyled = styled.div`
   display: flex;
   flex-direction: column;
@@ -12,7 +13,7 @@ const NotFoundPageWrapperStyled = styled.div`
     height: 7.5rem;
     flex-direction: column;
     justify-content: space-between;
-    margin: 35px 0 68px 0;
+    margin: 35px 0 68px;
 
     h2 {
       ${FONTS.h2}
