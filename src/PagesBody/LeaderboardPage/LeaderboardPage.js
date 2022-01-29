2b49321d3e128b07df954fda271feca4421434aa
Robert <elgreen@tlen.pl>
2022-01-29 16:01:40 +0100

    feat: component searches in mock database using 2 props as input

--- src/PagesBody/LeaderboardPage/LeaderboardPage.js
+++ src/PagesBody/LeaderboardPage/LeaderboardPage.js
@@ -1,21 +1,61 @@
 import ColumnWrapper from '../../Templates/ColumnWrapper/ColumnWrapper';
-import LeaderboardClassResult from './../../Data/Dummy-data/leaderboardClassResults';
 import LeaderboardList from './../../Molecules/LeaderboardList/LeaderboardList';
 import MainButtonStyled from './../../Atoms/MainButton/MainButton';
+import contestsRS from '../../Data/MongoDBMock/contestsRS';
+import individualSummaryInCurrentCompetiton from '../../Data/MongoDBMock/summaryResults';
+
+const fakeId = 'a0347677-c3c9-4edc-9d46-fed4a958fdc2';
+const fakeClass = '1';
 
 const LeaderboardPage = () => {
-  // below method sorts the array in descending order
-  // class0 results are in LeaderboardClassResults[0]
-  // classid -> class 0 -> [0]
-  const sortedLeaderboardClassResults = LeaderboardClassResult[0].sort(
-    (a, b) => {
-      return b.score - a.score;
-    },
+  // contestId: 'a0347677-c3c9-4edc-9d46-fed4a958fdc2',
+
+  // console.log(contests);RS
+  const fakeContest = contestsRS.find((contest) => contest.id === fakeId);
+  // console.log(fakeContest);
+  // console.log(fakeContest.obedienceClasses);
+  const fakeClassResult = fakeContest.obedienceClasses.find(
+    (classObj) => classObj.classNumber === fakeClass,
   );
+  // console.log(fakeClassResult);
+  // console.log(fakeClassResult.competingPairs);
+  let resultsIdArr = fakeClassResult.competingPairs.map(
+    (object) => object.summaryId,
+  );
+  console.log(resultsIdArr);
+
+  // const fakeDogSummaryId = '22-03-22Woof';
+  // const fakeDogSummary = individualSummaryInCurrentCompetiton.find(
+  //   (summary) => summary.competingPairsId === fakeDogSummaryId,
+  // );
+  // console.log(fakeDogSummary);
+  // const fakeDogResult = {
+  //   text: fakeDogSummary.dogName,
+  //   score: fakeDogSummary.summaryResult,
+  // };
+
+  // console.log(fakeDogResult);
+  const finalLeaderboardArr = resultsIdArr.map((summaryId) => {
+    let fakeDogSummary = individualSummaryInCurrentCompetiton.find(
+      (summary) => summary.competingPairsId === summaryId,
+    );
+    return {
+      text: fakeDogSummary.dogName,
+      score: fakeDogSummary.summaryResult,
+    };
+  });
+
+  // below method sorts the array in descending order
+  const sortedLeaderboardClassResults = finalLeaderboardArr.sort(
+    (a, b) => b.score - a.score,
+  );
+  console.log(finalLeaderboardArr);
   return (
     <ColumnWrapper paddingLeftRight={1}>
       <h1>I am leaderboard page</h1>
+      {/* props contestId, props classNumber */}
       <LeaderboardList result={sortedLeaderboardClassResults} />
+      {/* <LeaderboardList contestId={fakeId} classNumber={fakeClass} /> */}
       <br></br>
       <MainButtonStyled primary text="drukuj protokół" />
     </ColumnWrapper>
