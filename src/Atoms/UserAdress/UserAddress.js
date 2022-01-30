4b673a738a1cc72f3b6513a7c1dc1f9ce9d348c0
Tomasz <skretkowskitomasz@gmail.com>
2022-01-30 19:08:47 +0100

    - Added dynamic data mock

--- src/Atoms/UserAdress/UserAddress.js
+++ src/Atoms/UserAdress/UserAddress.js
@@ -1,10 +1,19 @@
+import PARTICIPANTS from '../../Data/Dummy-data/test-data-participants';
 import UserAddressStyled from './UserAddressStyled';
 
 const UserAddress = () => {
+  // userId otrzymane z contextu lub propsa
+
+  // TODO co w przypadku kiedy nie znajdziemy id ?
+
+  const userId = '1a0b079c-3d93-4fc0-a391-114e0f1d2004';
+  let userObject = PARTICIPANTS.find(
+    (participant) => participant.id === userId,
+  );
   return (
     <UserAddressStyled>
-      <p>Street</p>
-      <p>Zip-Code Town</p>
+      <p>{`${userObject.street} ${userObject.numberOfHosue}`}</p>
+      <p>{`${userObject.postalCode} ${userObject.city}`}</p>
     </UserAddressStyled>
   );
 };
