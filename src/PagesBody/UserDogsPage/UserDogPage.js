ebe4bc77b5a2a2155b3d812753912b10a814f45d
Alicja Kondrat <pierwszazlewej@gmail.com>
2022-02-02 21:13:49 +0100

    - attempt to make buttons active

--- src/PagesBody/UserDogsPage/UserDogPage.js
+++ src/PagesBody/UserDogsPage/UserDogPage.js
@@ -2,10 +2,11 @@ import ClassOrDogButton from '../../Molecules/ClassOrDogButton/ClassOrDogButton'
 import ColumnWrapper from '../../Templates/ColumnWrapper/ColumnWrapper';
 import MainButton from '../../Atoms/MainButton/MainButton';
 import participants from '../../Data/MongoDBMock/participants';
-
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const UserDogPage = () => {
+  const navigate = useNavigate();
+
   return (
     <ColumnWrapper paddingLeftRight={1} paddingTop={0.5}>
       {participants[0].dogs.map((dog, index) => {
@@ -17,10 +18,15 @@ const UserDogPage = () => {
               index,
               dogName,
             }}
+            onClick={console.log('spiesek')}
           />
         );
       })}
-      <MainButton secondary text="DODAJ PSIAKA" />
+      <MainButton
+        secondary
+        text="DODAJ PSIAKA"
+        onClick={() => navigate('/addDogForm')}
+      />
     </ColumnWrapper>
   );
 };
