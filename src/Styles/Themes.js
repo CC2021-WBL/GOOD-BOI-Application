7db9ccf0ff91a5868b9972f484f65f9ba52c3f5a
Alicja Kondrat <pierwszazlewej@gmail.com>
2022-02-04 21:55:26 +0100

    -dark mode colors

--- src/Styles/Themes.js
+++ src/Styles/Themes.js
@@ -29,19 +29,19 @@ export const lightTheme = {
   positive100: '#CBF1D9',
 };
 export const darkTheme = {
-  primary801: '#D5E6FB',
-  primary601: '#ACCCF8',
-  primary501: '#2F80ED',
-  primary201: '#1264D1',
+  primary801: '#b0d4ff',
+  primary601: '#509cff',
+  primary501: '#3b7dd4',
+  primary201: '#103d77',
   primary101: '#093269',
 
-  grey800: '#F5F7FA',
-  grey400: '#E4E7EB',
+  grey800: '#838383',
+  grey400: '#c5c5c5',
   grey300: '#9AA5B1',
-  grey200: '#7B8794',
+  grey200: '#484e55',
   grey100: '#596B7C',
-  grey00: '#323F4B',
-  white: '#1F2933',
+  grey00: '#272e36',
+  white: '#181e27',
 
   warning600: '#775E0D',
   warning400: '#E7B820',
