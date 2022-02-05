7b8499c264540e2fabb9916c3adac4dfaddabef3
Alicja Kondrat <pierwszazlewej@gmail.com>
2022-02-05 19:28:23 +0100

    - modal working

--- src/Atoms/ButtonsExercises/ButtonsExercises.js
+++ src/Atoms/ButtonsExercises/ButtonsExercises.js
@@ -1,26 +1,28 @@
 import ButtonExercisesStyled from './ButtonExercisesStyled';
 import { FaChevronLeft } from 'react-icons/fa';
 import propTypes from 'prop-types';
-import { useNavigate } from 'react-router-dom';
+
+// import { useNavigate } from 'react-router-dom';
 
 //Related to https://github.com/CC2021-WBL/GOOD-BOI-Application/pull/202/files#r798045688, this shuold receive only information about what theme color you want to apply. color={endingButton? "primary":"secondary"}
 
 const ButtonExercises = (props) => {
-  const { primary, secondary, text } = props;
-  const navigate = useNavigate();
+  // eslint-disable-next-line react/prop-types
+  const { primary, secondary, text, handler } = props;
+  // const navigate = useNavigate();
 
-  const handleClick = () => {
-    if (secondary) {
-      navigate(-1);
-    } else {
-      navigate('./dog-summary', {
-        state: { text: 'Tabela Wyników', label: 'Ocena Zawodnika' },
-      });
-    }
-  };
+  // const handleClick = () => {
+  //   if (secondary) {
+  //     navigate(-1);
+  //   } else {
+  //     navigate('./dog-summary', {
+  //       state: { text: 'Tabela Wyników', label: 'Ocena Zawodnika' },
+  //     });
+  //   }
+  // };
   return (
     <ButtonExercisesStyled
-      onClick={handleClick}
+      onClick={handler}
       primary={primary}
       secondary={secondary}
     >
