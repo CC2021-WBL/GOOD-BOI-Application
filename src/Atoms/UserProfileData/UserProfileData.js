f632625bc242dcf106da3abb08c7831594f2c8f9
Tomasz <skretkowskitomasz@gmail.com>
2022-02-05 22:42:48 +0100

    - Refactored: authentication test out of useEffect

--- src/Atoms/UserProfileData/UserProfileData.js
+++ src/Atoms/UserProfileData/UserProfileData.js
@@ -1,18 +1,44 @@
 import propTypes from 'prop-types';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import UserProfileDataStyled from './UserProfileDataStyled';
 import participants from '../../Data/MongoDBMock/participants';
 import { UserDataContext } from '../../Context/UserDataContext';
 
+const initialData = {
+  address: {
+    country: 'Polska',
+    city: 'Sfornegacie',
+    street: 'ul.Pszczelna',
+    numberOfHouse: '27/5',
+    postalCode: '50-124',
+  },
+};
+
 const UserProfileData = () => {
   const { state } = useContext(UserDataContext);
-  const { userId, userName, userSurname } = state;
-  const userObject = participants.find(
-    (participant) => participant.participantId === userId,
-  );
+  const { userId, userName, userSurname, isAuthenticated } = state;
+  const [userObject, setUserObject] = useState(initialData);
+
+  if (!isAuthenticated) {
+    throw new Error('Your not allowed to be here!');
+  }
+
   const { address } = userObject;
   const { street, numberOfHouse, city, postalCode } = address;
+
+  // mock for fetching data from database and checking if response has data
+
+  useEffect(() => {
+    const userObject = participants.find(
+      (participant) => participant.participantId === userId,
+    );
+    if (userObject === undefined) {
+      throw new Error('Fetch was unsuccessful');
+    } else {
+      setUserObject(userObject);
+    }
+  }, []);
 
   return (
     <UserProfileDataStyled>
