92e53547e4382edc367665fd26c89c2b793f58b2
Smyku6 <smyku6@gmail.com>
2022-02-08 19:35:37 +0100

    added two way navigation: to participant-data or to user-dogs

--- src/Organisms/DogData/DogData.js
+++ src/Organisms/DogData/DogData.js
@@ -9,38 +9,43 @@ import SpecialButton from '../../Atoms/SpecialButton/SpecialButton';
 import SpecialButtonsContainerStyled from '../../Molecules/SpecialButtonsContainer/SpecialButtonsContainerStyled';
 import { useContext, useEffect, useState } from 'react';
 import { UserDataContext } from '../../Context/UserDataContext';
+import { DogContext } from '../../Context/DogContext';
 
 const DogData = ({ id }) => {
   let navigate = useNavigate();
   const [dogData, setDogData] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const { state } = useContext(UserDataContext);
+  const { dogState } = useContext(DogContext);
+  console.log(dogState.chosenDog);
 
   useEffect(() => {
     setDogData(doggos.find((dog) => dog.dogId === id));
     setIsPending(false);
+    console.log(dogState);
   }, []);
 
   const handleEdit = (event) => {
     event.preventDefault();
-    navigate(`/add-dog-form/${id}`, {
+    navigate(`/add-dog-form/`, {
       state: {
         text: 'Formularz - edycja',
         label: `${dogData.dogName}`,
-        dogId: id,
       },
     });
   };
 
   const handleConfirm = (event) => {
     event.preventDefault();
-    navigate(`/participant-data/${state.userId}`, {
-      state: {
-        text: 'Dane zawodnika',
-        label: `${state.userName} ${state.userSurname}`,
-        participantId: state.userId,
-      },
-    });
+    !dogState.chosenDog &&
+      navigate(`/participant-data/${state.userId}`, {
+        state: {
+          text: 'Dane zawodnika',
+          label: `${state.userName} ${state.userSurname}`,
+          participantId: state.userId,
+        },
+      });
+    dogState.chosenDog && navigate(`/user-dogs`);
   };
 
   return (
@@ -64,7 +69,6 @@ const DogData = ({ id }) => {
     </ColumnWrapper>
   );
 };
-
 DogData.propTypes = {
   id: PropTypes.string.isRequired,
 };
