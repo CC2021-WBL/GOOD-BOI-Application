c78873e54af0918abfc3a1d01e2ae0d4c131f9b7
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-02-08 22:13:06 +0100

    Changed logo and added margin

--- src/PagesBody/LandingPage/LandingPage.js
+++ src/PagesBody/LandingPage/LandingPage.js
@@ -1,12 +1,15 @@
 import ColumnWrapper from '../../Templates/ColumnWrapper/ColumnWrapper';
 import FakeButton from '../../Atoms/FakeButton/FakeButton';
 import Footer from '../../Molecules/Footer/Footer';
-import LogoGoodBoi from '../../Atoms/LogoGoodBoi/LogoGoodBoi';
+import ImgWrapperStyled from './ImgWrapperStyled';
+import Logo2 from '../../Assets/Logo2.png';
 
 const LandingPage = () => {
   return (
     <ColumnWrapper paddingLeftRight={1}>
-      <LogoGoodBoi />
+      <ImgWrapperStyled>
+        <img src={Logo2} alt="App logo" />
+      </ImgWrapperStyled>
       <FakeButton to="/login" primary="primary" text="Zaloguj się" />
 
       <FakeButton to="/register" secondary="secondary" text="Zarejestruj" />
