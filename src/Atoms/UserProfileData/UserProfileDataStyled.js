a166d603153a2c4f60e882e03a07e1aaf1f9f937
Tomasz <skretkowskitomasz@gmail.com>
2022-02-09 19:05:59 +0100

    -Fixed: styles

--- src/Atoms/UserProfileData/UserProfileDataStyled.js
+++ src/Atoms/UserProfileData/UserProfileDataStyled.js
@@ -5,7 +5,11 @@ const UserProfileDataStyled = styled.div`
   flex-direction: column;
   text-align: left;
   padding: 0.625rem 0;
-  margin-right: ${(props) => (props.withEdit ? '0' : '40px')};
+
+  border-bottom: ${(props) =>
+    props.withEdit ? `1px solid ${props.theme.grey300}` : 'none'};
+
+  margin-right: ${(props) => (props.withEdit ? '0' : '2.5rem')};
 
   h3 {
     color: ${({ theme }) => theme.grey800};
@@ -23,6 +27,7 @@ const UserProfileDataStyled = styled.div`
     border: none;
     background-color: ${({ theme }) => theme.white};
     align-self: flex-end;
+    margin: 0 0 0.625rem;
 
     p {
       color: ${({ theme }) => theme.grey400};
