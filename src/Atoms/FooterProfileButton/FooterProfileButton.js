a741f0d63b809d54d1af8e8e7c80b3ff60f1fda2
Tomasz <skretkowskitomasz@gmail.com>
2022-02-09 19:41:01 +0100

    -Fixed: added redirection to profile in Footer

--- src/Atoms/FooterProfileButton/FooterProfileButton.js
+++ src/Atoms/FooterProfileButton/FooterProfileButton.js
@@ -1,15 +1,19 @@
 import propTypes from 'prop-types';
 import { FaUserCircle } from 'react-icons/fa';
 import { MdSettings } from 'react-icons/md';
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { Div, FooterProfileButtonStyled } from './FooterProfileButtonStyled';
+import { UserDataContext } from '../../Context/UserDataContext';
 
 const FooterProfileButton = ({ withSettings }) => {
+  const { state } = useContext(UserDataContext);
+  const { userId } = state;
   const navigate = useNavigate();
   function clickHandler() {
     {
-      withSettings ? navigate('./settings') : navigate('./user');
+      withSettings ? navigate('./settings') : navigate(`./user/${userId}`);
     }
   }
   return (
