a382f17c9e4c267f7173346733f40f3b5be9bfc2
Tomasz <skretkowskitomasz@gmail.com>
2022-02-09 20:09:02 +0100

    -Fixed: after logged user landing page with only one button

--- src/PagesBody/LandingPage/LandingPage.js
+++ src/PagesBody/LandingPage/LandingPage.js
@@ -1,21 +1,37 @@
+import { useContext } from 'react';
+
 import ColumnWrapper from '../../Templates/ColumnWrapper/ColumnWrapper';
 import FakeButton from '../../Atoms/FakeButton/FakeButton';
 import Footer from '../../Molecules/Footer/Footer';
 import ImgWrapperStyled from './ImgWrapperStyled';
 import Logo2 from '../../Assets/Logo2.png';
+import { UserDataContext } from '../../Context/UserDataContext';
 
 const LandingPage = () => {
+  const { state } = useContext(UserDataContext);
+  const { isAuthenticated } = state;
   return (
     <ColumnWrapper paddingLeftRight={1}>
       <ImgWrapperStyled>
         <img src={Logo2} alt="App logo" />
       </ImgWrapperStyled>
-      <FakeButton to="/login" primary="primary" text="Zaloguj się" />
-
-      <FakeButton to="/register" secondary="secondary" text="Zarejestruj" />
-
-      <FakeButton to="/in-progress" ternary="ternary" text="portal good boi" />
-
+      {isAuthenticated ? (
+        <FakeButton
+          to="/in-progress"
+          ternary="ternary"
+          text="portal good boi"
+        />
+      ) : (
+        <>
+          <FakeButton to="/login" primary="primary" text="Zaloguj się" />
+          <FakeButton to="/register" secondary="secondary" text="Zarejestruj" />
+          <FakeButton
+            to="/in-progress"
+            ternary="ternary"
+            text="portal good boi"
+          />
+        </>
+      )}
       <Footer />
     </ColumnWrapper>
   );
