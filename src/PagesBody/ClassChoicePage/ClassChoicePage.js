73e7fc048d24b7acabbe73991dc6de17f70070b5
Robert <elgreen@tlen.pl>
2022-02-10 13:58:44 +0100

    feat: added ClassChoicePage

---
+++ src/PagesBody/ClassChoicePage/ClassChoicePage.js
@@ -0,0 +1,47 @@
+import { useContext, useState } from 'react';
+
+import CLASSES from '../../Consts/classesConst';
+import ColumnWrapper from '../../Templates/ColumnWrapper/ColumnWrapper';
+import { ContestContext } from '../../Context/ContestContext';
+import FakeButton from '../../Atoms/FakeButton/FakeButton';
+import MainButton from '../../Atoms/MainButton/MainButton';
+
+const ClassChoicePage = () => {
+  const { contestState } = useContext(ContestContext);
+  const { contestId } = contestState;
+  console.log(contestState);
+  const classesArr = CLASSES.map((elem) => elem.name);
+  const [selectedClass, setSelectedClass] = useState('');
+  const clickHandler = (index) => {
+    setSelectedClass(index);
+  };
+
+  const linkTo = () => {
+    if (selectedClass !== undefined) {
+      return `../contests/${contestId}/classes/${selectedClass}/leaderboard`;
+    } else {
+      return '';
+    }
+  };
+
+  return (
+    <ColumnWrapper paddingLeftRight={1} paddingTop={0.25}>
+      {Object.keys(classesArr).map((obedienceClass, index) => {
+        return (
+          <MainButton
+            onClick={() => clickHandler(index)}
+            key={index}
+            style={{ height: '75px' }}
+            text={`Klasa ${obedienceClass}`}
+            ternary
+            justifyText={'left'}
+            className="selected-btn"
+          />
+        );
+      })}
+      <FakeButton text={'Pokaż wyniki'} secondary="secondary" to={linkTo()} />
+    </ColumnWrapper>
+  );
+};
+
+export default ClassChoicePage;
