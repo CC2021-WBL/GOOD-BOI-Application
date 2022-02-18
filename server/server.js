61faa8bef34c3866157bab6b11f1c474138e9d64
Smyku6 <smyku6@gmail.com>
2022-02-18 23:01:57 +0100

    verceltest

--- server/server.js
+++ server/server.js
@@ -7,12 +7,14 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const swaggerUI = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
+const path = require("path");
 
 const PORT = process.env.PORT || 27020;
 
 //Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client/public")))
 // app.use(helmet());
 // app.use(cookieParser());
 
@@ -20,6 +22,7 @@ app.use(express.json());
 const contestsRoute = require('./ServerRoutes/contests');
 const userRoute = require('./ServerRoutes/users');
 const dogsRoute = require('./ServerRoutes/dogs');
+
 
 //Connect to DB
 dotenv.config();
@@ -60,8 +63,12 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerOption
 
 
 //Routes
-app.get('/', (req, res) => {
-  res.send('Homepage');
+// app.get('/', (req, res) => {
+//   res.send('Homepage');
+// });
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/public", "index.html"));
 });
 
 app.listen(PORT, () => console.log(`The server is running on the port ${PORT}`));
