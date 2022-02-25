3323503eca773bc07ad81abe615d6f5254f4dc5a
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-02-25 17:44:53 +0100

    added trial versions of path

--- client/src/PagesBody/ManagerPage/ManagerPage.js
+++ client/src/PagesBody/ManagerPage/ManagerPage.js
@@ -8,13 +8,32 @@ import FakeButton from '../../Atoms/FakeButton/FakeButton';
 const ManagerPage = () => {
   // const { state } = useContext(UserDataContext);
   // const { isAuthenticated } = state;
-  return (
+    return (
     <ColumnWrapper paddingLeftRight={1}>
       {/* {isAuthenticated ? (
         <> */}
-      <FakeButton colors="ternary" text="Dodaj nowe zawody" to="/" />
-      <FakeButton colors="ternary" text="Zaplanowane zawody" to="/" />
-      <FakeButton colors="ternary" text="Twoje archiwalne zawody" to="/" />
+      <FakeButton
+        colors="ternary"
+        text="Dodaj nowe zawody"
+        to="/manager/:managerId/add-contest"
+        state={{ text: 'Formularz', label: 'Dodaj zawody' }}
+      />
+      <FakeButton
+        colors="ternary"
+        text="Zaplanowane zawody"
+        to="/contests"
+        state={{
+          contestContent: 'future',
+        }}
+      />
+      <FakeButton
+        colors="ternary"
+        text="Twoje archiwalne zawody"
+        to="/contests"
+        state={{
+          contestContent: 'present_and_past',
+        }}
+      />
       {/* </>
       ) : (
         <FakeButton colors="primary" text="Zaloguj się" to="/login" />
