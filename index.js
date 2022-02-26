be30c0639e383b74d07447be07f08a3ecfe5968f
Smyku6 <smyku6@gmail.com>
2022-02-27 00:21:04 +0100

    new approach

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const swaggerUI = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const path = require('path');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 27020;
 
 //Middleware
 app.use(cors());
