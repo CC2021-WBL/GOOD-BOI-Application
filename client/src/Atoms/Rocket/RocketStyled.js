2eee19c0c35c31e1ec75f98ccbf4339667d7ad74
Alicja Kondrat <pierwszazlewej@gmail.com>
2022-02-27 17:59:42 +0100

    - some changes

--- client/src/Atoms/Rocket/RocketStyled.js
+++ client/src/Atoms/Rocket/RocketStyled.js
@@ -1,9 +1,6 @@
 import styled from 'styled-components';
 
 const RocketStyled = styled.div`
-  transform: translate(0%);
-  transition: 0.3s ease-out;
-
   &:hover {
     transform: translate(-5%, -30%);
     transition: 0.3s ease-out;
