134380190e7245d2bd7bae1ebcf771868c534204
Smyku6 <smyku6@gmail.com>
2022-03-03 22:49:50 +0100

    added new swagger file

--- server/Routes/contests.js
+++ server/Routes/contests.js
@@ -3,66 +3,6 @@ const { registerContest } = require('../Controllers/contestControllers');
 const Contest = require('../Model/Contest');
 const router = express.Router();
 
-/**
- * @swagger
- * components:
- *   schemas:
- *     Contest:
- *       type: object
- *       properties:
- *         _id:
- *           type: string
- *           description: The auto-generated id of the contest
- *           example: '3845029d-e97d-41ed-997f-2299d09ef648'
- *         contestName:
- *           type: string
- *           description: The name of the contest
- *           example: 'Piętnasty zjazd dobrych chłopaków'
- *         kennelClubDepartment:
- *           type: string
- *           description: The city of kennelClubDepartment
- *           example: 'Warszawa'
- *         startDate:
- *           type: string
- *           format: date-time
- *           example: '1985-04-12T23:20:50.52Z'
- *
- *
- */
-
-/**
- * @swagger
- * /api/contests:
- *   get:
- *     summary: Returns the list of contests
- *     responses:
- *       200:
- *         description: The list of all contests
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Contest'
- *
- */
-
-/**
- * @swagger
- * /api/contests/classes/6219286509b1d60f7893e4a7:
- *  get:
- *    summary: Returns the list of classes in current contest
- *    responses:
- *      200:
- *        description: The list of all classes in current contest
- *        content:
- *          application/json:
- *            schema:
- *              type: array
- *              items:
- *                $ref: '#/components/schemas/Contest'
- */
-
 // Get all contests
 // router.get("/", async (req, res) => {
 //   try {
@@ -90,11 +30,11 @@ const router = express.Router();
 // #get classes for current contest -
 router.get('/classes/:contestId', async (req, res) => {
   try {
-    const classes = await Contest.findById(req.params.contestId).select(
-      'obedienceClasses',
-    );
-    if (classes) {
-      res.status(200).send(classes);
+    const { obedienceClasses } = await Contest.findById(
+      req.params.contestId,
+    ).select('obedienceClasses');
+    if (obedienceClasses) {
+      res.status(200).send(obedienceClasses);
     } else {
       res.status(404).json({ message: 'no class for current contest' });
     }
