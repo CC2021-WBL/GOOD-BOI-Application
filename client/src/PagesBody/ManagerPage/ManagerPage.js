b46fd79b465e3bcc7bdf83ef4d41e82280bfa5a5
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-03-05 19:26:07 +0100

    Added more padding

--- client/src/PagesBody/ManagerPage/ManagerPage.js
+++ client/src/PagesBody/ManagerPage/ManagerPage.js
@@ -1,43 +1,41 @@
 import ColumnWrapper from '../../Templates/ColumnWrapper/ColumnWrapper';
 import FakeButton from '../../Atoms/FakeButton/FakeButton';
+import { UserDataContext } from '../../Context/UserDataContext';
+import { useContext } from 'react';
 
-// import { UserDataContext } from '../../Context/UserDataContext';
-// import { useContext } from 'react';
-
-//context isAuthentic userData
 const ManagerPage = () => {
-  // const { state } = useContext(UserDataContext);
-  // const { isAuthenticated } = state;
-    return (
-    <ColumnWrapper paddingLeftRight={1}>
-      {/* {isAuthenticated ? (
-        <> */}
-      <FakeButton
-        colors="ternary"
-        text="Dodaj nowe zawody"
-        to="/manager/:managerId/add-contest"
-        state={{ text: 'Formularz', label: 'Dodaj zawody' }}
-      />
-      <FakeButton
-        colors="ternary"
-        text="Zaplanowane zawody"
-        to="/contests"
-        state={{
-          contestContent: 'future',
-        }}
-      />
-      <FakeButton
-        colors="ternary"
-        text="Twoje archiwalne zawody"
-        to="/contests"
-        state={{
-          contestContent: 'present_and_past',
-        }}
-      />
-      {/* </>
+  const { state } = useContext(UserDataContext);
+  const { isAuthenticated, userId } = state;
+  return (
+    <ColumnWrapper paddingLeftRight={1} className="manager-page">
+      {isAuthenticated ? (
+        <>
+          <FakeButton
+            colors="ternary"
+            text="Dodaj nowe zawody"
+            to={`/manager/${userId}/add-contest`}
+            state={{ text: 'Formularz', label: 'Dodaj zawody' }}
+          />
+          <FakeButton
+            colors="ternary"
+            text="Zaplanowane zawody"
+            to="/contests"
+            state={{
+              contestContent: 'future',
+            }}
+          />
+          <FakeButton
+            colors="ternary"
+            text="Twoje archiwalne zawody"
+            to="/contests"
+            state={{
+              contestContent: 'results',
+            }}
+          />
+        </>
       ) : (
         <FakeButton colors="primary" text="Zaloguj się" to="/login" />
-      )} */}
+      )}
     </ColumnWrapper>
   );
 };
