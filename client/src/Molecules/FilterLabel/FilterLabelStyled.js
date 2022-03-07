7d9070115b9b708bc8c255c77716e23e0ee0d595
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-03-07 20:43:22 +0100

    Changed styling filter labels

--- client/src/Molecules/FilterLabel/FilterLabelStyled.js
+++ client/src/Molecules/FilterLabel/FilterLabelStyled.js
@@ -13,14 +13,19 @@ const FilterLabelStyled = styled.label`
     align-items: center;
     justify-content: space-around;
     background-color: ${({ theme }) => theme.grey100};
-    
   }
 
   @media only screen and (min-width: 800px) {
     margin-top: 0;
-    div{border-radius: 0 0 0.75rem 0.75rem;
+    div {
+      border-radius: 0 0 0.75rem 0.75rem;
+      background-color: ${({ theme }) => theme.grey00};
+      justify-content: flex-end;
       .innerWrapper {
         max-width: 37.5rem;
+        width: 65%;
+        gap: 0.625rem;
+        margin-right: 0.625rem;
       }
     }
   }
