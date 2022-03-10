b84d130d76da2892f7f1b279c63ce908845ab045
Tomasz <skretkowskitomasz@gmail.com>
2022-03-10 19:53:07 +0100

    Fixed: Grid columns set width for first column

--- client/src/Templates/Layout/Grid.js
+++ client/src/Templates/Layout/Grid.js
@@ -10,6 +10,9 @@ const Grid = styled.div`
     grid-template-columns: 1fr 3fr;
     grid-template-rows: 110px 85px 3fr 110px;
   }
+  @media only screen and (min-width: 1024px) {
+    grid-template-columns: 220px 1fr;
+  }
   @media only screen and (min-width: 1600px) {
     grid-template-columns: 1fr 220px minmax(360px, 1160px) 220px 1fr;
     grid-template-rows: 110px 85px 3fr 240px;
