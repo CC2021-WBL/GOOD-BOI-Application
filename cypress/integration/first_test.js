0d48e27d2a9531bd7da882a3a1db9701f94cb013
Smyku6 <smyku6@gmail.com>
2022-03-12 16:08:04 +0100

    added new devices

--- cypress/integration/first_test.js
+++ cypress/integration/first_test.js
@@ -1,11 +1,51 @@
-describe('My First Test', () => {
-  it('Log in and log out', () => {
-    cy.visit(Cypress.env('server'));
-    cy.contains('Zaloguj się').click();
-    cy.url().should('include', '/login');
-    cy.get('input[id="email"]').type(Cypress.env('randomUserLogin'));
-    cy.get('input[id="password"]').type(Cypress.env('randomUserPassword'));
-    cy.contains('ZALOGUJ SIĘ').click();
-    cy.contains('WYLOGUJ SIĘ').click();
+import test_devices from './test_devices';
+
+test_devices.forEach(({ type, width, height }) => {
+  describe(`First GoodBoi Test ${type}`, () => {
+    beforeEach(() => {
+      cy.viewport(width, height);
+    });
+
+    it('LandingPage', () => {
+      cy.visit(Cypress.env('server'));
+
+      cy.get('.fakebtn-header')
+        .should('contain', 'Zaloguj się')
+        .and('contain', 'Zarejestruj')
+        .and('contain', 'portal good boi');
+    });
+
+    it('LoginPage', () => {
+      cy.contains('Zaloguj się').click();
+
+      cy.get('.navText').should('contain', 'Logowanie');
+      cy.get('label').should('contain', 'Email').and('contain', 'Password');
+      cy.get('#email').should('have.attr', 'placeholder', ' Email');
+      cy.get('#password').should('have.attr', 'placeholder', ' Hasło');
+      cy.get('.forgot-pass').should('contain', 'Zapomniałeś/aś hasła ?');
+      cy.get('button').should('contain', 'ZALOGUJ SIĘ');
+    });
+
+    it('Logging in', () => {
+      cy.get('#email').type(Cypress.env('zosiaUserLogin'));
+      cy.get('#password').type(Cypress.env('zosiaUserPassword'));
+      cy.contains('ZALOGUJ SIĘ').click();
+    });
+
+    it('UserPage', () => {
+      cy.contains('KONTO ZAWODNIKA').click();
+
+      cy.get('.desktop_only').should(
+        'contain',
+        'Przejdź do formularza zapisu na zawody.',
+      );
+    });
+
+    it('Logging out', () => {
+      if (type === 'figma mobile') {
+        cy.get('.burger-wrapper').click();
+      }
+      cy.contains('Wyloguj się').click();
+    });
   });
 });
---
+++ cypress/integration/test_devices.js
@@ -0,0 +1,7 @@
+const test_devices = [
+  { type: 'figma mobile', width: 360, height: 640 },
+  { type: 'figma tablet', width: 768, height: 1024 },
+  { type: 'figma desktop', width: 1440, height: 1024 },
+];
+
+export default test_devices;
