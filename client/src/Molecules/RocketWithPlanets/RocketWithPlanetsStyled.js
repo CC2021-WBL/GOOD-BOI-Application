2f2423e4aca2576cab621ceabe864639d4c8e6e6
Alicja Kondrat <pierwszazlewej@gmail.com>
2022-03-12 18:22:29 +0100

    - enchancements after CR

--- client/src/Molecules/RocketWithPlanets/RocketWithPlanetsStyled.js
+++ client/src/Molecules/RocketWithPlanets/RocketWithPlanetsStyled.js
@@ -2,14 +2,12 @@ import planets from '../../Assets/planets.png';
 import styled from 'styled-components';
 
 const RocketWithPlanetsStyled = styled.div`
+  display: none;
   @media only screen and (min-width: 1600px) {
     display: block;
     grid-area: 4 / 4 / 5 / 5;
     background-image: url(${planets});
     background-repeat: no-repeat;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
     position: relative;
     bottom: 4.9375rem;
     left: 9.375rem;
