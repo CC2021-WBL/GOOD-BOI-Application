73202de50d4b11fe8b4e19f93488ec9aef60ecd5
Tomasz <skretkowskitomasz@gmail.com>
2022-03-17 22:30:34 +0100

    Added: Error boundary styles

--- client/src/Tools/ErrorBoundary.js
+++ client/src/Tools/ErrorBoundary.js
@@ -1,4 +1,5 @@
 import propTypes from 'prop-types';
+import styled from 'styled-components';
 import { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
@@ -30,59 +31,64 @@ export class ErrorBoundary extends Component {
   render() {
     if (this.state.hasError) {
       return (
-        <>
+        <BodyWrapper>
           <NotFoundPageWrapperStyled style={{ textAlign: 'center' }}>
             <img src={ErrorPageGraphic} alt="Doggo in space" />
-            <div className="mainContentWrapper">
+            <div className="text-wrapper">
               <h1>Coś poszło nie tak.</h1>
               <br />
               <p>Wróć do strony głównej i spróbuj ponownie później.</p>
             </div>
-            <NavLink
-              style={{
-                margin: '1rem',
-                width: '30%',
-                height: '3.25rem',
-                borderRadius: '10px',
-                textDecoration: 'none',
-                color: 'grey',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                border: '1px solid grey',
-                fontSize: '0.9375rem',
-              }}
-              onClick={this.refresh}
-              to="/"
-            >
+            <NavLink className="error-link" onTouchTap={this.refresh} to="/">
               Odśwież stronę!
             </NavLink>
             <NavLink
-              style={{
-                margin: '1rem',
-                width: '30%',
-                height: '3.25rem',
-                borderRadius: '10px',
-                textDecoration: 'none',
-                color: 'grey',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                border: '1px solid grey',
-                fontSize: '0.9375rem',
-              }}
+              className="error-link error-link__contact"
               onClick={this.refresh}
               to="/contact-form"
             >
               Skontaktuj się z nami!
             </NavLink>
           </NotFoundPageWrapperStyled>
-        </>
+        </BodyWrapper>
       );
     }
     return this.props.children;
   }
 }
+
+const BodyWrapper = styled.div`
+  height: 100vh;
+  background-color: ${({ theme }) => theme.white};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .text-wrapper {
+    color: ${({ theme }) => theme.grey800};
+  }
+
+  .error-link {
+    margin: 1rem;
+    width: 100%;
+    height: 3.25rem;
+    border-radius: 10px;
+    text-decoration: none;
+    color: grey;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid grey;
+    font-size: 0.9375rem;
+  }
+
+  .error-link__contact {
+    color: ${({ theme }) => theme.white};
+    border: 0.5px solid ${({ theme }) => theme.grey800};
+    background-color: ${({ theme }) => theme.primary501};
+  }
+`;
 
 ErrorBoundary.propTypes = {
   children: propTypes.node,
