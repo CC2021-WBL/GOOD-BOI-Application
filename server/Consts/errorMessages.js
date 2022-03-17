1ffa61ea40ee60d5b2af63e819f538116c10900f
Tomasz <skretkowskitomasz@gmail.com>
2022-03-17 21:04:43 +0100

    -Added: error messages consts

---
+++ server/Consts/errorMessages.js
@@ -0,0 +1,8 @@
+module.exports.ERROR_MSG = {
+  400: 'Ojej! Spokojnie, nic się nie stało. Sprawdź dokładnie wpisany adres. Spróbuj odświeżyć przeglądarkę.',
+  401: 'Ojej! Autoryzacja nieudana. Spróbuj zalogować się ponownie.',
+  403: 'Ojej! Nie masz uprawnień do tej części serwisu.',
+  404: 'Ojej! Ta strona nie istnieje. Spróbuj jeszcze raz.',
+  500: 'Ojej! Nasz serwer przechodzi przez pewien etap. Daj mu czas i spróbuj ponownie później.',
+  503: 'Ojej! Usługa jest niedostępna. Skontaktuj się z nami.',
+};
