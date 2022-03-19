f90544eab50311f81505fc5b456ba78d4d01f34f
Monika Krella <monika.krella@gmail.com>
2022-03-19 22:26:51 +0100

    very beginning of successfull render map

--- client/src/Organisms/Map/Map.js
+++ client/src/Organisms/Map/Map.js
@@ -1,22 +1,28 @@
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 import ReactMap from 'react-map-gl';
 import { useState } from 'react';
 
 const Map = () => {
-  const [viewport, setViewport] = useState({
+  const [viewstate, setViewstate] = useState({
     latitude: 54.3611929,
-    longitude: 18.6286088,
-    zoom: 10,
-    width: '100vh',
-    height: '100vh',
+    longitude: 18.62,
+    zoom: 20,
   });
   return (
-    <div>
-      {' '}
-      <ReactMap
-        {...viewport}
-        mapboxAccessToken="pk.eyJ1IjoibW9uaWtha3IiLCJhIjoiY2wweTZ6NjYyMXF2bTNpbm1sb2FzYnU1ayJ9.82jjXRoyf1BwTpQkFRmTZw"
-      ></ReactMap>
-    </div>
+    <ReactMap
+      initialViewState={{
+        latitude: 51.796,
+        longitude: 19.426,
+        zoom: 4.5,
+      }}
+      style={{
+        width: 380,
+        height: 420,
+      }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken="pk.eyJ1IjoibW9uaWtha3IiLCJhIjoiY2wweTZ6NjYyMXF2bTNpbm1sb2FzYnU1ayJ9.82jjXRoyf1BwTpQkFRmTZw"
+    ></ReactMap>
   );
 };
 
