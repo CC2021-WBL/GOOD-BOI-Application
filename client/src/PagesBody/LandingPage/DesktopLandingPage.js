9fc24b683f7ec5cc33987af6b7933352ade6736e
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-03-19 18:01:37 +0100

    Improved fetch data

--- client/src/PagesBody/LandingPage/DesktopLandingPage.js
+++ client/src/PagesBody/LandingPage/DesktopLandingPage.js
@@ -1,34 +1,37 @@
-import { useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import ColumnWrapper from '../../Templates/ColumnWrapper/ColumnWrapper';
 import ContestCard from '../../Molecules/ContestCard/ContestCard';
 import ContestsWrapperStyled from '../ContestsPage/ContestsWrapperStyled';
 import FakeButton from '../../Atoms/FakeButton/FakeButton';
-import { TIME } from '../../Consts/infoLabelConsts';
 import TitleWrapperStyled from '../../Molecules/LandingDesktopTitle/TitleWrapperStyled';
-import { getSelectedContestsByTime } from '../../Tools/TimeFunctions';
 import mockmap from '../../Assets/mockMAP.JPG';
-import { requestOptionsGET } from '../../Tools/FetchData/requestOptions';
-import resForContestPage from '../../Data/MongoDBMock/responseFromContestsToContestsPage';
+import { TIME } from '../../Consts/infoLabelConsts';
+import { UserDataContext } from '../../Context/UserDataContext';
+import { getContestsCards } from '../../Tools/FetchData/fetchContestsfunctions';
+import { getSelectedContestsByTime } from '../../Tools/TimeFunctions';
+import { removeNullsFromArray } from '../../Tools/FetchData/additionalToolsForResults';
 
 const DesktopLandingPage = () => {
-  const [isPending, setIsPending] = useState(true);
-  const [contestData, setContestData] = useState(null);
-  const [selectedMode, setSelectedMode] = useState(null);
   const rawDataFromDB = useRef(null);
-
+  const [isPending, setIsPending] = useState(true);
+  const locationPath = useLocation();
+  const [selectedMode, setSelectedMode] = useState(null);
+  const { state } = useContext(UserDataContext);
   useEffect(() => {
-    fetch('/api/contests/?taker=card', requestOptionsGET)
-      .then((response) => response.json())
-      .then((result) => {
-        rawDataFromDB.current = result;
-        console.log(rawDataFromDB.current);
-      })
-      .catch((error) => console.log('error', error));
+    async function fetchContestsData() {
+      let result = await getContestsCards(state, locationPath);
 
-    setContestData(resForContestPage);
-    setSelectedMode(TIME.FUTURE);
-    setIsPending(false);
+      if (Array.isArray(result)) {
+        result = removeNullsFromArray(result);
+      }
+      rawDataFromDB.current = result;
+
+      setIsPending(false);
+      setSelectedMode(TIME.FUTURE);
+    }
+    fetchContestsData();
   }, []);
 
   return (
@@ -53,8 +56,9 @@ const DesktopLandingPage = () => {
           </TitleWrapperStyled>
           <h3 className="incoming-contests">NADCHODZĄCE KONKURSY</h3>
           {isPending && <h3>Loading...</h3>}
-          {contestData &&
-            getSelectedContestsByTime(selectedMode, contestData).map(
+
+          {rawDataFromDB.current &&
+            getSelectedContestsByTime(selectedMode, rawDataFromDB.current).map(
               (contest) => (
                 <ContestCard
                   key={contest.contestId}
