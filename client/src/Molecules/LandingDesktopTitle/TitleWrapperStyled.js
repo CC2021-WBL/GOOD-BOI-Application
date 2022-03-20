7e7b9e47a426f3fd37ae12f31953d0c4147ddd8d
Alicja Kondrat <pierwszazlewej@gmail.com>
2022-03-20 20:17:06 +0100

    - landing page upadee

--- client/src/Molecules/LandingDesktopTitle/TitleWrapperStyled.js
+++ client/src/Molecules/LandingDesktopTitle/TitleWrapperStyled.js
@@ -1,14 +1,13 @@
-import styled from 'styled-components';
-
 import FONTS from '../../Styles/fontsStyledComponents';
+import styled from 'styled-components';
 
 const TitleWrapperStyled = styled.div`
   display: flex;
   max-width: 37.5rem;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: center;
   margin: -3.9375rem 0 0 0;
-  text-align: left;
+  text-align: center;
   h1 {
     color: ${({ theme }) => theme.grey800};
     font-size: 3rem;
