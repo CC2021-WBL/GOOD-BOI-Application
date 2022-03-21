0c05767ded89b4ecc9f85375329da859cda7589d
Smyku6 <smyku6@gmail.com>
2022-03-21 01:49:50 +0100

    initial commit

--- client/src/Templates/ColumnWrapper/ColumnWrapperStyled.js
+++ client/src/Templates/ColumnWrapper/ColumnWrapperStyled.js
@@ -29,6 +29,7 @@ const ColumnWrapperStyled = styled.div`
   &.role-page-column-wrapper {
     margin: 0 auto;
   }
+
   .login-form-captions {
     display: none;
     color: ${({ theme }) => theme.grey800};
@@ -36,10 +37,12 @@ const ColumnWrapperStyled = styled.div`
     text-align: left;
     margin: 0.5rem;
   }
+
   .login-form-header {
     ${FONTS.h2};
     line-height: 2.5;
   }
+
   .login-form-centered {
     text-align: center;
   }
@@ -60,20 +63,24 @@ const ColumnWrapperStyled = styled.div`
     &.user-dogs-column-wrapper {
       align-items: flex-end;
     }
+
     &.user-profile {
       gap: 1.25rem;
     }
+
     &.contests-column-wrapper {
       display: flex;
       max-width: 42.5rem;
       margin: 1.5625rem 0 0 0;
     }
+
     &.user-data-wrapper {
       border: 1px solid ${({ theme }) => theme.grey200};
       border-radius: 0.9375rem;
       margin: 0 auto;
       height: fit-content;
     }
+
     &.login-column-wrapper {
       max-width: 470px;
       border: 1px solid ${({ theme }) => theme.grey200};
@@ -87,6 +94,7 @@ const ColumnWrapperStyled = styled.div`
     .login-form-captions {
       display: block;
     }
+
     @media only screen and (min-width: 1024px) {
       &.user-dogs-column-wrapper {
         align-items: center;
@@ -100,24 +108,51 @@ const ColumnWrapperStyled = styled.div`
         grid-template-columns: repeat(8, 1fr);
         grid-template-rows: repeat(2, 10.625rem) 1fr;
       }
+
       &.user-data-wrapper {
         margin: 0 auto;
         margin-bottom: 1rem;
       }
+
       &.contests-column-wrapper {
         display: flex;
         max-width: 42.5rem;
         margin: 1.5625rem 0 0 0;
       }
+
       &.login-column-wrapper {
         max-width: 29.375rem;
         border: 1px solid ${({ theme }) => theme.grey200};
         border-radius: 0.9375rem;
         margin: auto;
       }
+
       .login-form-captions {
         display: block;
         background-color: ${({ theme }) => theme.white};
+      }
+
+      &.dog-data-container {
+        max-width: 35rem;
+        border: 1px solid ${({ theme }) => theme.grey200};
+        border-radius: 0.9375rem;
+        margin: 1.5rem auto auto;
+        display: flex;
+        flex-direction: column-reverse;
+      }
+
+      &.dog-data-details {
+        margin-bottom: 1rem;
+      }
+
+      &.dog-data-details-bar {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 3rem;
+        border-radius: 0.75rem 0.75rem 0 0;
+        background-color: ${({ theme }) => theme.grey200};
       }
 
       @media only screen and (min-width: 1600px) {
@@ -130,9 +165,11 @@ const ColumnWrapperStyled = styled.div`
       grid-template-columns: 1fr 1fr;
       grid-template-rows: 1fr;
     }
+
     &.contest-data-details {
       grid-area: 1/1/2/2;
     }
+
     &.contest-data-buttons {
       display: flex;
       flex-direction: row;
