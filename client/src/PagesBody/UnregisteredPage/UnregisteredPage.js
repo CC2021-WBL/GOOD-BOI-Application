bf23308d2bd9fbea2dd13d98663e5bd6e2e538cd
Monika Krella <monika.krella@gmail.com>
2022-03-22 17:46:14 +0100

    -small change for rebuild of vercel

--- client/src/PagesBody/UnregisteredPage/UnregisteredPage.js
+++ client/src/PagesBody/UnregisteredPage/UnregisteredPage.js
@@ -3,19 +3,6 @@ import { useEffect, useState } from 'react';
 import ColumnWrapper from '../../Templates/ColumnWrapper/ColumnWrapper';
 import Map from '../../Organisms/Map/Map';
 
-// const contestsCoordiantes = [
-//   {
-//     contestName: 'Zawody Pana Kleksa',
-//     latitude: 52.248,
-//     longitude: 20.976,
-//   },
-//   {
-//     contestName: 'Mistrzostwa Zjadaczy Kabanosów',
-//     latitude: 51.168,
-//     longitude: 22.472,
-//   },
-// ];
-
 const mapSize = {
   width: 380,
   height: 420,
