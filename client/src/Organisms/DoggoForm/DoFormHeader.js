5391fc88cd19d5eab4383106a39617699af607c5
Robert <elgreen@tlen.pl>
2022-03-22 07:07:25 +0100

    fix: added media query

--- client/src/Organisms/DoggoForm/DoFormHeader.js
+++ client/src/Organisms/DoggoForm/DoFormHeader.js
@@ -1,40 +1,44 @@
 import styled from 'styled-components';
 
 const DogFormHeaderStyled = styled.div`
-  margin-top: 2rem;
+  display: none;
+  margin-top: 1rem;
   text-align: left;
 
-  h2 {
-    margin-bottom: 1rem;
-  }
-  caption {
-    text-align: left;
-    white-space: nowrap;
-    margin-bottom: 2rem;
-  }
-  .baloon {
-    font-weight: 900;
-    display: flex;
-    border-radius: 50%;
-    border: 1px solid ${({ theme }) => theme.grey200};
-    width: 2rem;
-    height: 2rem;
-    background: none;
-    justify-content: center;
-    align-items: center;
-    font-size: 15px;
-    color: ${({ theme }) => theme.grey200};
-  }
-  .active {
-    background: ${({ theme }) => theme.primary101};
-    border: 3px solid ${({ theme }) => theme.primary601};
-    color: ${({ theme }) => theme.primary601};
-  }
-  .line {
-    height: 2px;
-    width: 1rem;
-    background: ${({ theme }) => theme.grey200};
-    align-self: center;
+  @media only screen and (min-width: 800px) {
+    display: block;
+    h2 {
+      margin-bottom: 1rem;
+    }
+    caption {
+      text-align: left;
+      white-space: nowrap;
+      margin-bottom: 2rem;
+    }
+    .baloon {
+      font-weight: 900;
+      display: flex;
+      border-radius: 50%;
+      border: 1px solid ${({ theme }) => theme.grey200};
+      width: 2rem;
+      height: 2rem;
+      background: none;
+      justify-content: center;
+      align-items: center;
+      font-size: 15px;
+      color: ${({ theme }) => theme.grey200};
+    }
+    .active {
+      background: ${({ theme }) => theme.primary101};
+      border: 3px solid ${({ theme }) => theme.primary601};
+      color: ${({ theme }) => theme.primary601};
+    }
+    .line {
+      height: 2px;
+      width: 1rem;
+      background: ${({ theme }) => theme.grey200};
+      align-self: center;
+    }
   }
 `;
 
