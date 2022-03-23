1d292785c9d4655dbb24d0f048600e8e188564b7
Tomasz <skretkowskitomasz@gmail.com>
2022-03-23 22:51:08 +0100

    -In progress

--- client/src/PagesBody/ErrorPage/ErrorPage.js
+++ client/src/PagesBody/ErrorPage/ErrorPage.js
@@ -1,18 +1,13 @@
 import ErrorPageGraphic from '../../Assets/ErrorPageGraphic.png';
-import NotFoundPageWrapperStyled from '../../Organisms/NotFoundContent/NotFoundPageWrapperStyled';
 
-const ErrorPage = ({ ResStatus }) => {
+const ErrorPage = ({ message }) => {
   return (
-    <NotFoundPageWrapperStyled style={{ textAlign: 'center' }}>
+    <>
       <img src={ErrorPageGraphic} alt="Doggo in space" />
       <div className="text-wrapper">
-        <h1>Coś poszło nie tak.</h1>
-        {/* W zależności od podanego statusu wyświetla się konkretna wiadomość
-        na każdej stronie wypada dodać regułę która przenosi razem ze zwróconym 
-        statusem na error page i wyświetla konkretną wiadomość */}
-        {/* <p>{ERROR_MSG.ResStatus}</p> */}
+        <h1>{message}</h1>
       </div>
-    </NotFoundPageWrapperStyled>
+    </>
   );
 };
 
